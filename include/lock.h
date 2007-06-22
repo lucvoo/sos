0416d9979ba3280104ce98dab25026e21b6834b3
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-06-22 11:30:04 +0200

    LOCK: Add (spin)lock implementation

---
+++ include/lock.h
@@ -0,0 +1,71 @@
+#ifndef	_LOCK_H_
+#define	_LOCK_H_
+
+#ifndef CONFIG_SMP
+struct lock {
+	// FIXME: SMP
+};
+
+
+#define LOCK_UNLOCKED	(struct lock) { }
+#define DEFINE_LOCK(x)	struct lock x = LOCK_UNLOCKED
+
+static inline void lock_init(struct lock *lock)
+{
+	*lock = LOCK_UNLOCKED;
+}
+
+static inline void __lock_acq(struct lock *lock)
+{
+	(void) lock;
+}
+
+static inline void __lock_rel(struct lock *lock)
+{
+	(void) lock;
+}
+#endif
+
+
+#include <irqflags.h>
+
+static inline void lock_acq(struct lock *lock)
+{
+	__lock_acq(lock);
+}
+
+static inline void lock_acq_irq(struct lock *lock)
+{
+	__local_irq_dis();
+	__lock_acq(lock);
+}
+
+static inline unsigned long __attribute__((warn_unused_result)) lock_acq_save(struct lock *lock)
+{
+	unsigned long flags;
+
+	flags = __local_irq_save();
+	__lock_acq(lock);
+
+	return flags;
+}
+
+
+static inline void lock_rel(struct lock *lock)
+{
+	__lock_rel(lock);
+}
+
+static inline void lock_rel_irq(struct lock *lock)
+{
+	__lock_rel(lock);
+	__local_irq_ena();
+}
+
+static inline void lock_rel_rest(struct lock *lock, unsigned long flags)
+{
+	__lock_rel(lock);
+	__local_irq_rest(flags);
+}
+
+#endif
