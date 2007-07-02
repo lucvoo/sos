195d4d627b9723deade17c8d3abb57cef904d47a
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-07-02 17:04:22 +0200

    CORE: add definition of struct frame

--- arch/arm/include/thread.h
+++ arch/arm/include/thread.h
@@ -14,4 +14,25 @@ struct cpu_context {
 	unsigned long	pc;
 };
 
+struct eframe {
+	unsigned long	r0;
+	unsigned long	r1;
+	unsigned long	r2;
+	unsigned long	r3;
+	unsigned long	r4;
+	unsigned long	r5;
+	unsigned long	r6;
+	unsigned long	r7;
+	unsigned long	r8;
+	unsigned long	r9;
+	unsigned long	r10;		// sl
+	unsigned long	fp;
+	unsigned long	ip;
+	unsigned long	sp;
+	unsigned long	lr;
+	unsigned long	pc;
+	unsigned long	cpsr;
+	unsigned long	old_r0;
+};
+
 #endif
