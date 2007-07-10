031902450301e428edb965d90acfd7a4d3948e04
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-07-10 17:32:07 +0200

    ARM: Add definition of SCTLR bits

--- arch/arm/include/arch.h
+++ arch/arm/include/arch.h
@@ -1,6 +1,33 @@
 #ifndef _ARCH_ARCH_H_
 #define _ARCH_ARCH_H_
 
+/*
+ * SCTLR bits (CP#15 CR1)
+ */
+#define	SCTLR_M		(1 << 0)	// MMU enable
+#define	SCTLR_A		(1 << 1)	// Alignment abort enable
+#define	SCTLR_C		(1 << 2)	// Dcache enable
+#define	SCTLR_W		(1 << 3)	// Write buffer enable
+#define	SCTLR_P		(1 << 4)	// 32-bit exception handler
+#define	SCTLR_D		(1 << 5)	// 32-bit data address range
+#define	SCTLR_L		(1 << 6)	// Implementation defined
+#define	SCTLR_B		(1 << 7)	// Big endian
+#define	SCTLR_S		(1 << 8)	// System MMU protection
+#define	SCTLR_R		(1 << 9)	// ROM MMU protection
+#define	SCTLR_F		(1 << 10)	// Implementation defined
+#define	SCTLR_Z		(1 << 11)	// Implementation defined
+#define	SCTLR_I		(1 << 12)	// Icache enable
+#define	SCTLR_V		(1 << 13)	// Vectors relocated to 0xffff0000
+#define	SCTLR_RR	(1 << 14)	// Round Robin cache replacement
+#define	SCTLR_L4	(1 << 15)	// LDR pc can set T bit
+#define	SCTLR_DT	(1 << 16)
+#define	SCTLR_IT	(1 << 18)
+#define	SCTLR_ST	(1 << 19)
+#define	SCTLR_FI	(1 << 21)	// Fast interrupt (lower latency mode)
+#define	SCTLR_U		(1 << 22)	// Unaligned access operation
+#define	SCTLR_XP	(1 << 23)	// Extended page tables
+#define	SCTLR_VE	(1 << 24)	// Vectored interrupts
+
 //
 // CPSR & SPSR
 //
