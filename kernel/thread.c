afb1ce4a14e3a4aed7bbf8fd5a02549b56290c7c
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-07-17 02:55:31 +0200

    SCHED: Add context switching of a newly created thread

--- kernel/thread.c
+++ kernel/thread.c
@@ -1,5 +1,18 @@
 #include <thread.h>
+#include <sched.h>
 
+
+static void thread_entry(void)
+{
+	struct thread* t = get_current_thread();
+	void (*func)(void*);
+	void* data;
+
+	thread_need_resched_clear(t);
+	thread_initial_context(t, func, data);
+
+	func(data);
+}
 
 int thread_create(struct thread* t, int priority, void (*entry_func)(void*), void* entry_data)
 {
@@ -8,6 +21,8 @@ int thread_create(struct thread* t, int priority, void (*entry_func)(void*), voi
 	t->priority	= priority;
 	t->flags	= TIF_NEED_RESCHED;
 	t->state	= THREAD_STATE_SLEEPING;
+
+	thread_load_context(t, entry_func, entry_data, thread_entry);
 
 	return 0;
 }
