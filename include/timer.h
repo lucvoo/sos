3799e3dd8ea22c1674d8dd4fdf5454c233a33561
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-07-20 17:35:46 +0200

    TIMER: Move the timer code to its own file

---
+++ include/timer.h
@@ -0,0 +1,6 @@
+#ifndef	_TIMER_H_
+#define	_TIMER_H_
+
+#include <hz.h>
+
+#endif
--- init/main.c
+++ init/main.c
@@ -5,12 +5,14 @@
 void __clear_bss(void);
 void _os_initcalls(void);
 void platform_init(void);
+void timer_init(void);
 
 void _os_start(void)
 {
 	__clear_bss();
 	_os_initcalls();
 	platform_init();	// FIXME: arch_initcall
+	timer_init();		// FIXME: subsys_initcall
 	kapi_start();
 	_thread_scheduler_start();
 
