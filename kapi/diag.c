9380393b247c6e5b7f44eec2b94c30ec4dfc18d7
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-07-20 17:37:11 +0200

    DIAG: Make diag_write_buf() & diag_write_string() atomic vs interrupts

--- kapi/diag.c
+++ kapi/diag.c
@@ -1,18 +1,26 @@
 #include <diag.h>
 #include <symbols.h>
+#include <irqflags.h>
 
 
 void _os_diag_write_string(const char* str)
 {
 	unsigned int c;
+	unsigned long flags;
 
+	flags = __local_irq_save();
 	while ((c = *str++))
 		_os_diag_write_char(c);
+	__local_irq_rest(flags);
 }
 
 void _os_diag_write_buf(const char* str, unsigned int n)
 {
+	unsigned long flags;
+
+	flags = __local_irq_save();
 	while (n--)
 		_os_diag_write_char(*str++);
+	__local_irq_rest(flags);
 }
 weak_alias(_os_diag_write_buf, putx);
