964e17562fa45477876358414e2957105cf252cd
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2007-12-13 17:09:14 +0100

    ARM: remove unused and redundant defines

--- arch/arm/include/arch.h
+++ arch/arm/include/arch.h
@@ -1,33 +1,6 @@
 #ifndef _ARCH_ARCH_H_
 #define _ARCH_ARCH_H_
 
-/*
- * SCTLR bits (CP#15 CR1)
- */
-#define	SCTLR_M		(1 << 0)	// MMU enable
-#define	SCTLR_A		(1 << 1)	// Alignment abort enable
-#define	SCTLR_C		(1 << 2)	// Dcache enable
-#define	SCTLR_W		(1 << 3)	// Write buffer enable
-#define	SCTLR_P		(1 << 4)	// 32-bit exception handler
-#define	SCTLR_D		(1 << 5)	// 32-bit data address range
-#define	SCTLR_L		(1 << 6)	// Implementation defined
-#define	SCTLR_B		(1 << 7)	// Big endian
-#define	SCTLR_S		(1 << 8)	// System MMU protection
-#define	SCTLR_R		(1 << 9)	// ROM MMU protection
-#define	SCTLR_F		(1 << 10)	// Implementation defined
-#define	SCTLR_Z		(1 << 11)	// Implementation defined
-#define	SCTLR_I		(1 << 12)	// Icache enable
-#define	SCTLR_V		(1 << 13)	// Vectors relocated to 0xffff0000
-#define	SCTLR_RR	(1 << 14)	// Round Robin cache replacement
-#define	SCTLR_L4	(1 << 15)	// LDR pc can set T bit
-#define	SCTLR_DT	(1 << 16)
-#define	SCTLR_IT	(1 << 18)
-#define	SCTLR_ST	(1 << 19)
-#define	SCTLR_FI	(1 << 21)	// Fast interrupt (lower latency mode)
-#define	SCTLR_U		(1 << 22)	// Unaligned access operation
-#define	SCTLR_XP	(1 << 23)	// Extended page tables
-#define	SCTLR_VE	(1 << 24)	// Vectored interrupts
-
 //
 // CPSR & SPSR
 //
@@ -59,5 +32,36 @@
 #define PSR_s			0x00ff0000	// Status
 #define PSR_x			0x0000ff00	// Extension
 #define PSR_c			0x000000ff	// Control
+
+
+//
+// SCTLR bits (CP#15 CR1)
+//
+#define	SCTLR_M		(1 << 0)	// MMU enable
+#define	SCTLR_A		(1 << 1)	// Alignment check enable
+#define	SCTLR_C		(1 << 2)	// D/U cache enable
+#define	SCTLR_W		(1 << 3)	// Write buffer enable		(< ARMv7, RAO/SBOP for ARMv7)
+#define	SCTLR_BE	(1 << 5)	// CP15 dmb, dsb & isb enabled	(ARMv7, optional)
+#define	SCTLR_B		(1 << 7)	// Big endian			(< ARMv7, ARMv7 RAZ/SBOP)
+#define	SCTLR_S		(1 << 8)	// System MMU protection	(< ARMv7, RAZ/SBOP for ARMv7)
+#define	SCTLR_R		(1 << 9)	// ROM MMU protection		(< ARMv7, RAZ/SBOP for ARMv7)
+#define	SCTLR_SW	(1 << 10)	// SWP & SWPB enable
+#define	SCTLR_Z		(1 << 11)	// branch prediction enable
+#define	SCTLR_I		(1 << 12)	// Icache enable
+#define	SCTLR_V		(1 << 13)	// high Vectors
+#define	SCTLR_RR	(1 << 14)	// Round Robin cache replacement
+#define	SCTLR_DT	(1 << 16)	//				(< ARMv7, RAO/SBOP)
+#define	SCTLR_HA	(1 << 17)	// Hardware Access flag		(ARMv7)
+#define	SCTLR_WXN	(1 << 19)	//				(if virtualization)
+#define	SCTLR_UWXN	(1 << 20)	//				(if virtualization)
+#define	SCTLR_FI	(1 << 21)	// Fast Interrupt (lower latency mode)
+#define	SCTLR_U		(1 << 22)	// Unaligned access operation	(< ARMv7, RAO/SBOP)
+#define	SCTLR_XP	(1 << 23)	// eXtended Page tables		(< ARMv7)
+#define	SCTLR_VE	(1 << 24)	// VEctored interrupts
+#define	SCTLR_EE	(1 << 25)	// Exception Endianness
+#define	SCTLR_NMFI	(1 << 27)	// Non-Maskable FIQ
+#define	SCTLR_TRE	(1 << 28)	// Tex Remap Enable
+#define	SCTLR_AFE	(1 << 29)	// Access Flag Enable
+#define	SCTLR_TE	(1 << 30)	// Thumb Exception enable
 
 #endif
