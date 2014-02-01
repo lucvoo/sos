acc8db93c16ed5b9075d0c3d21a1082ee2cd93b3
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-02-01 15:28:13 +0100

    KBUILD: Use config to specify address of RAM and such so that it can be used in make commands too

--- arch/arm/include/memory.h
+++ arch/arm/include/memory.h
@@ -4,4 +4,11 @@
 #include <asm-ul.h>
 #include "arch/mach/memory.h"
 
+#ifndef	VIRT_ADDR
+#define	VIRT_ADDR	UL(CONFIG_VIRT_ADDR)		// Virtual RAM. Same as Linux's PAGE_OFFSET
+#endif
+#ifndef	TEXT_OFFSET
+#define	TEXT_OFFSET	UL(CONFIG_TEXT_OFFSET)
+#endif
+
 #endif
--- arch/arm/mach-beaglebone/include/memory.h
+++ arch/arm/mach-beaglebone/include/memory.h
@@ -1,7 +1,5 @@
 #ifndef	_ARCH_MACH_MEMORY_H_
 #define	_ARCH_MACH_MEMORY_H_
 
-#define	VIRT_ADDR	UL(0x80000000)			// Virtual RAM
-#define	TEXT_OFFSET	UL(0x00008000)
 
 #endif
