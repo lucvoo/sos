683ec7d9e7f311de0f9eed76e1dc9df0708fb0d0
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-02-04 18:30:32 +0100

    UTILS: add a small hexdump() implementation

---
+++ include/hexdump.h
@@ -0,0 +1,3 @@
+/* lib/hexdump.c */
+void hexdump(const void *buff, unsigned int len);
+void binhex(const void *addr, unsigned int len);
---
+++ lib/hexdump.c
@@ -0,0 +1,68 @@
+#include <hexdump.h>
+
+static inline unsigned int mapchar(unsigned int c)
+{
+	if (c >= ' ' && c <= '~')
+		return c;
+	else
+		return '.';
+}
+
+static void hexdump_line(const unsigned char *buff, unsigned int len, int ascii)
+{
+	unsigned int i;
+
+	printf("%p  ", buff);
+
+	if (1) {
+		// Hex part
+		for (i = 0; i < len; i++)
+			printf("%02x ", buff[i]);
+		for (; i < 16; i++)
+			printf("   ");
+	}
+
+	if (ascii) {
+		// Ascii part
+		for (i = 0; i < len; i++)
+			printf("%c", mapchar(buff[i]));
+		for (; i < 16; i++)
+			printf(" ");
+	}
+
+	printf("\n");
+}
+
+void hexdump(const void *buff, unsigned int len)
+{
+	// Dump the buffer by lines of maximum 16 bytes
+	while (len) {
+		unsigned int n = len > 16 ? 16 : len;
+
+		hexdump_line(buff, n, 0);
+		buff += n;
+		len -= n;
+	}
+}
+
+#if 0
+void hexdumpf(const void *addr, unsigned int len, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+
+	hexdump(addr, len);
+}
+#endif
+
+void binhex(const void *addr, unsigned int len)
+{
+	const unsigned char *src = addr;
+
+	while (len--)
+		printf("%02X", *src++);
+	printf("\n");
+}
