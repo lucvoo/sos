c566fb0f6002d63e1bcf03f19f7bfd148b7b7034
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-02-06 15:33:07 +0100

    ARM: use the newly defined symbolic names for cp15 registers

--- arch/arm/debug/dump_system_regs.c
+++ arch/arm/debug/dump_system_regs.c
@@ -1,42 +1,47 @@
+#include <arch/cp15.h>
+#include <stringify.h>
+
+
 #define	BYTE(V, N)	(((V) >> ((N)*8)) & 0xFF)
 
 #define	dump_reg(NAME, ASM)	\
 	do { unsigned long r; __asm__ __volatile__(ASM : "=r" (r)); printf("%s:\t%08X (%08b %08b %08b %08b)\n", NAME, r, BYTE(r,3), BYTE(r,2), BYTE(r,1), BYTE(r,0)); } while (0)
+#define	dump_cp15(NAME)	dump_reg(#NAME, "mrc " STRINGIFY(NAME(%0)))
 
 
 void dump_system_regs(void)
 {
 	printf("\n");
-	dump_reg("MIDR",  "mrc   p15, 0, %0, c0, c0, 0");
-	dump_reg("TCMTR", "mrc   p15, 0, %0, c0, c0, 2");
-	dump_reg("TLBTR", "mrc   p15, 0, %0, c0, c0, 3");
-	dump_reg("MPIDR", "mrc   p15, 0, %0, c0, c0, 5");
-	dump_reg("REVIDR","mrc   p15, 0, %0, c0, c0, 6");
+	dump_cp15(MIDR);
+	dump_cp15(TCMTR);
+	dump_cp15(TLBTR);
+	dump_cp15(MPIDR);
+	dump_cp15(REVIDR);
 
 	printf("\n");
-	dump_reg("CTR",   "mrc   p15, 0, %0, c0, c0, 1");
-	dump_reg("CCSIDR","mrc   p15, 1, %0, c0, c0, 1");
-	dump_reg("CLIDR", "mrc   p15, 1, %0, c0, c0, 7");
+	dump_cp15(CTR);
+	dump_cp15(CCSIDR);
+	dump_cp15(CLIDR);
 
 	printf("\n");
 	dump_reg("CPSR",  "mrs %0, cpsr");
-	dump_reg("SCTRL", "mrc   p15, 0, %0, c1, c0, 0");
-	dump_reg("VBAR",  "mrc   p15, 0, %0, c12, c0, 0");
+	dump_cp15(SCTLR);
+	dump_cp15(VBAR);
 
 	printf("\n");
-	dump_reg("TTBCR", "mrc   p15, 0, %0, c2, c0, 2");
-	dump_reg("TTBR0", "mrc   p15, 0, %0, c2, c0, 0");
-	dump_reg("TTBR1", "mrc   p15, 0, %0, c2, c0, 1");
-	dump_reg("PRRR",  "mrc   p15, 0, %0, c10, c2, 0");
-	dump_reg("NMRR",  "mrc   p15, 0, %0, c10, c2, 1");
+	dump_cp15(TTBCR);
+	dump_cp15(TTBR0);
+	dump_cp15(TTBR1);
+	dump_cp15(PRRR);
+	dump_cp15(NMRR);
 
 	printf("\n");
-	dump_reg("NSACR", "mrc   p15, 0, %0, c1, c1, 2");
-	//dump_reg("SCR",   "mrc   p15, 0, %0, c1, c1, 0");
+	dump_cp15(NSACR);
+	//dump_cp15(SCR);
 
 
 #if 0
 	printf("\n");
-	dump_reg("CNTFRQ","p15, 0, %0, c14, c0, 0");
+	dump_reg(CNTFRQ);
 #endif
 }
