ea597d9a3d5a886a2135bd17809752392dacbabc
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-02-09 15:15:52 +0100

    STDIO: Add missing prototype for vsnprintf()

--- include/printf.h
+++ include/printf.h
@@ -1,7 +1,11 @@
 #ifndef	_PRINTF_H_
 #define	_PRINTF_H_
 
+#include <stdarg.h>
+
 void printf(const char *fmt, ...);
+
 void snprintf(char *dest, unsigned size, const char *fmt, ...);
+void vsnprintf(char* dest, unsigned size, const char* fmt, va_list ap);
 
 #endif
