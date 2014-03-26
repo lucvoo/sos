3e4dbe5b63910d7e7ea65f2afd1fbf9a2f122767
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-03-26 03:22:04 +0100

    CORE: add definition of likely()/unlikely()

--- include/compiler.h
+++ include/compiler.h
@@ -1,6 +1,9 @@
 #ifndef	_COMPILER_H_
 #define	_COMPILER_H_
 
+#define	likely(x)		__builtin_expect(!!(x), 1)
+#define	unlikely(x)		__builtin_expect(!!(x), 0)
+
 #define __used			__attribute__((__used__))
 #define __section(name)		__attribute__((__section__(name)))
 
