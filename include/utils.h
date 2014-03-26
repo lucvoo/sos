17c9d5847e5d0edbbacc1bff4630530c87efd2e8
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-03-26 03:12:45 +0100

    CORE: Add definition of ARRAY_SIZE()

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
 #ifndef	_UTILS_H_
 #define	_UTILS_H_
 
+#define	ARRAY_SIZE(A)	(sizeof(A)/sizeof(A[0]))
+
 #define container_of(ptr, type, member) ({			\
         const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
         (type *)( (char *)__mptr - __builtin_offsetof(type,member) );})
