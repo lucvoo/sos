f50129af23a47616f70990507d806fd9760b16ad
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-03-29 05:38:14 +0100

    CORE: Add platform initialization

--- init/main.c
+++ init/main.c
@@ -4,11 +4,13 @@
 
 void __clear_bss(void);
 void _os_initcalls(void);
+void platform_init(void);
 
 void _os_start(void)
 {
 	__clear_bss();
 	_os_initcalls();
+	platform_init();	// FIXME: arch_initcall
 	kapi_start();
 	_thread_scheduler_start();
 
