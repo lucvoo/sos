4bd11216dd30846664e36e28b7541320574a283f
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-09-14 01:41:09 +0200

    MATH: Add an implemntation of sqrt()

---
+++ include/math.h
@@ -0,0 +1,7 @@
+#ifndef _MATH_H_
+#define _MATH_H_
+
+unsigned long sqrtl(unsigned long n);
+unsigned long sqrtll(unsigned long long n);
+
+#endif
---
+++ lib/sqrt.c
@@ -0,0 +1,43 @@
+#include <math.h>
+
+unsigned long sqrtl(unsigned long n)
+{
+	unsigned long r = 0;
+	unsigned long b = 1 << (sizeof(n) * 8 - 2);
+
+	// b = the highest power of 4 not greater than n
+	while (b > n)
+		b >>= 2;
+
+	while (b != 0) {
+		if (n >= r + b) {
+			n -= r + b;
+			r = (r >> 1) + b;
+		} else
+			r >>= 1;
+		b >>= 2;
+	}
+
+	return r;
+}
+
+unsigned long sqrtll(unsigned long long n)
+{
+	unsigned long long r = 0;
+	unsigned long long b = 1ULL << (sizeof(n) * 8 - 2);
+
+	// b = the highest power of 4 not greater than n
+	while (b > n)
+		b >>= 2;
+
+	while (b != 0) {
+		if (n >= r + b) {
+			n -= r + b;
+			r = (r >> 1) + b;
+		} else
+			r >>= 1;
+		b >>= 2;
+	}
+
+	return r;
+}
