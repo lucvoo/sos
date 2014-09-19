9d6039013a8ec81a4503ed8a03c3df309660b920
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-09-19 09:55:52 +0200

    DIAG: Add _os_diag_nl() & _os_diag_hexval()

--- include/diag.h
+++ include/diag.h
@@ -5,6 +5,11 @@ void _os_diag_write_char(unsigned int c);
 void _os_diag_write_string(const char* str);
 void _os_diag_write_buf(const char* str, unsigned int n);
 
+void _os_diag_hexval(unsigned long val);
+
 void _os_diag_printf(const char *fmt, ...);
+
+
+static void _os_diag_nl(void) { _os_diag_write_char('\n'); }
 
 #endif
---
+++ kapi/diag-hexval.c
@@ -0,0 +1,18 @@
+#include <diag.h>
+
+
+void _os_diag_hexval(unsigned long val)
+{
+	int n;
+
+	for (n = sizeof(val)*2; n--;) {
+		unsigned int d = (val >> (4*n)) & 0xf;
+
+		if (d > 9)
+			d += 'A' - 10;
+		else
+			d += '0';
+
+		_os_diag_write_char(d);
+	}
+}
