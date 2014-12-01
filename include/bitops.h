ab7460ff4cdd1777348d3073c6fb5fe2ff6d7249
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-12-01 16:36:24 +0100

    BITOPS: simplify/cleanify

--- include/bitops.h
+++ include/bitops.h
@@ -1,42 +1,29 @@
 #ifndef	_BITOPS_H_
 #define	_BITOPS_H_
 
-#include <arch/bitops.h>
+
+#define BITOP_MASK(bit)		(1UL << ((bit) % (sizeof(long)*8)))
+#define BITOP_WORD(bit)		((bit) / (sizeof(long)*8))
 
 
-#define BITOP_MASK(nr)		(1UL << ((nr) % (sizeof(long)*8)))
-#define BITOP_WORD(nr)		((nr) / (sizeof(long)*8))
-
-static inline void bitop_set(unsigned long *addr, int nr)
+static inline void bitop_set(unsigned long *addr, unsigned int bit)
 {
-	unsigned long mask = BITOP_MASK(nr);
-	unsigned long *p = addr + BITOP_WORD(nr);
-
-	*p  |= mask;
+	addr[BITOP_WORD(bit)] |= BITOP_MASK(bit);
 }
 
-static inline void bitop_clr(unsigned long *addr, int nr)
+static inline void bitop_clr(unsigned long *addr, unsigned int bit)
 {
-	unsigned long mask = BITOP_MASK(nr);
-	unsigned long *p = addr + BITOP_WORD(nr);
-
-	*p &= ~mask;
+	addr[BITOP_WORD(bit)] &= ~BITOP_MASK(bit);
 }
 
-static inline void bitop_not(unsigned long *addr, int nr)
+static inline void bitop_inv(unsigned long *addr, unsigned int bit)
 {
-	unsigned long mask = BITOP_MASK(nr);
-	unsigned long *p = addr + BITOP_WORD(nr);
-
-	*p &= mask;
+	addr[BITOP_WORD(bit)] ^= BITOP_MASK(bit);
 }
 
-static inline int bitop_get(const unsigned long *addr, int nr)
+static inline unsigned long bitop_get(const unsigned long *addr, unsigned int bit)
 {
-	unsigned long mask = BITOP_MASK(nr);
-	const unsigned long *p = addr + BITOP_WORD(nr);
-
-	return *p & mask;
+	return addr[BITOP_WORD(bit)] & BITOP_MASK(bit);
 }
 
 #endif
