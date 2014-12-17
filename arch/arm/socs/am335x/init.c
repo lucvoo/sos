8487375568a853a7b3631ac6853a9d9fdb3814a5
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-12-17 09:32:41 +0100

    AM335X: map the most important IO ranges

--- arch/arm/socs/am335x/init.c
+++ arch/arm/socs/am335x/init.c
@@ -1,8 +1,31 @@
 #include <init.h>
+#include <iomap.h>
+#include <utils/array-size.h>
+
+
+static struct iomap_desc iomap_tbl[] = {
+	{	// L4_PER
+		.phys = 0x48000000,
+		.size = 0x01000000,
+		.virt = 0xF0000000,
+	},
+	{	// L4_FAST
+		.phys = 0x4A000000,
+		.size = 0x01000000,
+		.virt = 0xF1000000,
+	},
+	{	// L4_WKUP
+		.phys = 0x44C00000,
+		.size = 0x00400000,
+		.virt = 0xF2000000,
+	},
+};
 
 
 static void am335x_init(void)
 {
 	printf("AM335x booting ...\n");
+
+	iomap_init(iomap_tbl, 3);
 }
 core_initcall(am335x_init);
