0bd18d1349d0dc3f83f71b4fa0a2a38cf6ffe2ca
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-12-18 02:31:55 +0100

    SYNC,ARM: use the gcc generic atomic ops

---
+++ arch/arm/include/atomic.h
@@ -0,0 +1,6 @@
+#ifndef	_ARCH_ATOMIC_H_
+#define	_ARCH_ATOMIC_H_
+
+#include <generic/atomic.h>
+
+#endif
---
+++ include/atomic.h
@@ -0,0 +1,6 @@
+#ifndef	_ATOMIC_H_
+#define	_ATOMIC_H_
+
+#include <arch/atomic.h>
+
+#endif
