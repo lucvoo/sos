57a8676d378b917caf63a2f7c3eb506e949af2ef
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-12-21 21:38:28 +0100

    ARM,MM: define struct page & its flags

--- include/page.h
+++ include/page.h
@@ -8,4 +8,27 @@
 #define	PAGE_MASK	(~(PAGE_SIZE-1))
 #define	PAGE_ALIGN(adr)	(((adr)+PAGE_SIZE-1)&PAGE_MASK)
 
+#ifndef	__ASSEMBLY__
+
+#include <dlist.h>
+
+enum page_flags {
+	PG_reserved	= 0x00000001,	// reserved for the kernel or ...
+	PG_free		= 0x00000002,	// free in the binary buddy
+};
+
+
+struct page {
+	unsigned long	flags;
+
+	unsigned int	order;		// order in the binary buddy (when PG_free)
+
+	struct dlist	list;
+};
+
+
+// WARNING: most of the time, changing and/or testing the flags need to be done atomically
+
+#endif
+
 #endif
