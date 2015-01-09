64029c8cca0dfd4c64c89002b3a7319e89754509
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-01-09 01:32:11 +0100

    MM,PAGEALLOC: fill the freelists with the unused pages

--- mm/pagemap.c
+++ mm/pagemap.c
@@ -1,5 +1,7 @@
 #include <page.h>
+#include <page-alloc.h>
 #include <string.h>
+#include <memory.h>
 #include <init.h>
 
 
@@ -7,9 +9,33 @@ extern struct page pagemap[];
 
 static void *heap_top;
 
+
+static unsigned int get_pfn_order(unsigned long pfn)
+{
+	return __builtin_ctz(pfn | (1 << MAX_ORDER));
+}
+
+static void __init pages_add(unsigned long pfn, unsigned long nbr)
+{
+	while (nbr) {
+		unsigned int order = get_pfn_order(pfn);
+		unsigned long size = 1 << order;
+
+		if (nbr < size)
+			order = get_pfn_order(nbr);
+
+		page_free(pfn_to_page(pfn), order);
+
+		pfn += size;
+		nbr -= size;
+	}
+}
+
+
 static int __init paging_init(unsigned long nbr)
 {
 	unsigned long size;
+	unsigned long pfn;
 
 	// "reserve" the storage for the pagemap
 	size = PAGE_ALIGN(nbr * sizeof(struct page));
@@ -17,6 +43,11 @@ static int __init paging_init(unsigned long nbr)
 
 	// and initialize it
 	memset(pagemap, 0, size);
+
+
+	// add all the unused pages to the page freelists
+	pfn = __phys_to_pfn(virt_to_phys(heap_top));
+	pages_add(pfn, nbr - (pfn - PFN_OFFSET));
 
 	return 0;
 }
