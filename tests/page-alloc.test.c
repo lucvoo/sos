0720dd1e6c7546f6dc4e3afc575cda816a7f6d70
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-01-12 17:57:49 +0100

    TEST,PAGEALLOC: improve testing page_alloc() & page_free()

--- tests/page-alloc.test.c
+++ tests/page-alloc.test.c
@@ -30,25 +30,23 @@ static unsigned long free_allocated_pages(unsigned int i)
 		n += 1 << order;
 	}
 
-	printf("%s(): free %lu pages\n", __func__, n);
+	//printf("%s(): free %lu pages\n", __func__, n);
 	return n;
 }
 
-static void fun(void* data)
+static int test_alloc_free_all(void)
 {
 	unsigned long n = 0;
 	unsigned int i = 0;
+	unsigned long f;
 
 	while (1) {
 		unsigned int order;
 		struct page *p;
 
-		do
-			order = sprng() & 0x3;
-		while (order > MAX_ORDER);
+		order = sprng() % 3;
 
 		p = page_alloc(order, 0);
-
 		if (p) {
 			n += 1 << order;
 			pages[i++] = ((struct pages) { p, order, });
@@ -56,22 +54,34 @@ static void fun(void* data)
 
 		//printf("page_alloc(%2u) => page:%p, pfn:%lx (tot:%lu)\n", order, p, page_to_pfn(p), n);
 
-		if (!p && order == 0) {
-			unsigned long f;
-
-			printf("no memory left! (nbr alloc = %u)\n", i);
-
-			f = free_allocated_pages(i);
-			if (f != n) {
-				printf("ERROR! f = %lu but n was %lu\n", f, n);
-				break;
-			}
-
-			n = 0;
-			i = 0;
-		}
+		if (!p && order == 0)
+			break;
 	}
 
+	//printf("DEBUG: no memory left! (nbr alloc = %u)\n", i);
+
+	f = free_allocated_pages(i);
+	if (f != n) {
+		printf("ERROR! %s(): f = %lu but n was %lu\n", __func__, f, n);
+		return 1;
+	}
+
+	return 0;
+}
+
+#define RESULT(X)	if (X) ko++; else ok++
+
+static void fun(void* data)
+{
+	unsigned int ok = 0;
+	unsigned int ko = 0;
+	int i;
+
+	for (i = 0; i < 64; i++) {
+		RESULT(test_alloc_free_all());
+	}
+
+	printf("TEST page-alloc: fail = %u, succeed = %i\n", ko, ok);
 	thread_sleep();
 }
 
