2f47a093507ad94f0648ec2430d9026173f1442b
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-02-13 03:57:34 +0100

    CORE: Add definition for pseudo qualifier "__unused"

--- include/compiler.h
+++ include/compiler.h
@@ -5,6 +5,7 @@
 #define	unlikely(x)		__builtin_expect(!!(x), 0)
 
 #define __used			__attribute__((__used__))
+#define __unused		__attribute__((__unused__))
 #define __section(name)		__attribute__((__section__(name)))
 #define __noreturn		__attribute__((noreturn))
 #define __uninit		__attribute__((section(".uninit")))
