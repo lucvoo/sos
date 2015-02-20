e1a94a9c04339609e04b278c269009ca27ed862e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-02-20 16:10:05 +0100

    MIPS: add some helper for functions written in assembly

--- arch/mips/include/assembly.h
+++ arch/mips/include/assembly.h
@@ -2,6 +2,23 @@
 #define	_ARCH_ASSEMBLY_H_
 
 #include <arch/regs-alias.h>
+
+
+#define	DEFFUN(name, framesize, ra)	\
+	.align	2;		\
+	.type	name, @function;\
+	.ent	name;		\
+name:	.frame	sp, framesize, ra
+
+#define	EXTFUN(name, framesize, ra)	\
+	.globl	name;			\
+	DEFFUN(name, framesize, ra)
+
+#define	ENDFUN(name)		\
+	.end	name;		\
+	.size	name, .-name
+
+
 #include <generic/assembly.h>
 
 #endif
