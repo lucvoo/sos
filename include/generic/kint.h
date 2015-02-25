9b706e1a33a04d92bb350fbb7bd9dd7cc4ec49e0
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-02-25 02:00:59 +0100

    CORE: Add definition of {s,u}{8,16,32,64}

---
+++ include/generic/kint.h
@@ -0,0 +1,13 @@
+#ifndef	_GENERIC_KINT_H_
+#define	_GENERIC_KINT_H_
+
+typedef __INT8_TYPE__		s8;
+typedef __INT16_TYPE__		s16;
+typedef __INT32_TYPE__		s32;
+typedef __INT64_TYPE__		s64;
+typedef __UINT8_TYPE__		u8;
+typedef __UINT16_TYPE__		u16;
+typedef __UINT32_TYPE__		u32;
+typedef __UINT64_TYPE__		u64;
+
+#endif
---
+++ include/types/kint.h
@@ -0,0 +1,6 @@
+#ifndef	_TYPES_KINT_H_
+#define	_TYPES_KINT_H_
+
+#include <generic/kint.h>
+
+#endif
