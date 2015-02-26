e60814067a96c2eb5ffa84b5ab80d69d65740d78
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-02-26 04:04:27 +0100

    PRINTF: printf() need exclusive access to the underlying HW

--- lib/printf.c
+++ lib/printf.c
@@ -1,6 +1,9 @@
 #include "xprintf.h"
 #include <printf.h>
 #include <stdarg.h>
+#include <lock.h>
+
+struct lock printf_lock;
 
 int putx(const char *buf, unsigned n);
 static int _putx(const char *buf, unsigned n, char *dest, unsigned size)
@@ -19,9 +22,12 @@ void vprintf(const char *fmt, va_list ap)
 
 void printf(const char *fmt, ...)
 {
+	unsigned long flags;
 	va_list ap;
 
 	va_start(ap, fmt);
+	flags = lock_acq_save(&printf_lock);
 	xprintf(_putx, NULL, -1, fmt, ap);
+	lock_rel_rest(&printf_lock, flags);
 	va_end(ap);
 }
