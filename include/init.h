245e33d5181d8854e98eba34871ef45f6b56a418
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-02 15:45:44 +0100

    INIT: Add more fined-grained levels for board_initcall()

--- include/init.h
+++ include/init.h
@@ -20,7 +20,12 @@ struct initcall {
 #define	mm_initcall(fn)			__initcall("2",fn)
 #define	core_initcall(fn)		__initcall("3",fn)
 #define	subsys_initcall(fn)		__initcall("4",fn)
-#define	board_initcall(fn)		__initcall("5",fn)
+#define	board_probe_initcall(fn)	__initcall("5a",fn)
+#define	board_setup_initcall(fn)	__initcall("5b",fn)
+#define	board_irq_initcall(fn)		__initcall("5c",fn)
+#define	board_core_initcall(fn)		__initcall("5d",fn)
+#define	board_initcall(fn)		__initcall("5e",fn)
+#define	board_late_initcall(fn)		__initcall("5f",fn)
 #define	device_initcall(fn)		__initcall("6",fn)
 #define	late_initcall(fn)		__initcall("7",fn)
 
