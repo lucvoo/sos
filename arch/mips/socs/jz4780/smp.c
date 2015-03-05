416a5dcfb0ef9d667301898cfa9633f1a6abc56d
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-05 01:56:55 +0100

    JZ4780: Add an IRQ handler for the inter-process mailboxes

--- arch/mips/socs/jz4780/smp.c
+++ arch/mips/socs/jz4780/smp.c
@@ -7,6 +7,61 @@
 #include <smp.h>
 
 
+/******************************************************************************/
+/******** IPI ********/
+
+#include <arch/hazard.h>
+#include <interrupt.h>
+#include <irqdesc.h>
+#include <lock.h>
+
+#define	IRQ_MBOX		3 // on MIPS internal irqs
+
+/* This lock protect the 'core' registers:
+ *	- c0_core_ctrl
+ *	- c0_core_stat
+ *	- c0_core_reim
+ *	- c0_mailbox[01]
+ */
+static struct lock core_regs_lock;
+
+/* The JZ4780's mailboxes are for sending short messages (a single word)
+ * between the cores.
+ * They are (used to implement) the Inter-Process Interrupts.
+ */
+static int mbox_irq_handler(struct irqdesc *desc, void *data)
+{
+	unsigned int msg;
+
+	lock_acq(&core_regs_lock);
+	switch (__coreid()) {
+	case 0:
+		msg = c0_getval(c0_mailbox0);
+		c0_setval(c0_mailbox0, 0);
+		c0_clrbits(c0_core_stat, 1 << 0);
+		break;
+	case 1:
+		msg = c0_getval(c0_mailbox1);
+		c0_setval(c0_mailbox1, 0);
+		c0_clrbits(c0_core_stat, 1 << 1);
+		break;
+	default:
+		// FIXME
+		msg = 0;
+		break;
+	}
+	lock_rel(&core_regs_lock);
+
+	// FIXME: do something with the message
+	(void) msg;
+
+	return IRQ_HANDLED;
+}
+
+static struct irqaction mbox_action;
+/******************************************************************************/
+
+
 static struct thread idle_thread_core1;
 
 struct thread *__smp_sec_cpu_thread;
@@ -19,6 +74,18 @@ void __smp_init(void)
 	unsigned long entry;
 	unsigned int gated;
 	unsigned int lpcr;
+
+	// setup the mailboxes' irq handler
+	irq_create(&mbox_action, mbox_irq_handler, NULL, NULL, 0);
+	irq_attach(irq_get_desc(NULL, IRQ_MBOX), &mbox_action);
+
+	c0_clrbits(c0_core_reim, 0x1);
+	c0_setval(c0_mailbox0, 0x0);
+	c0_setval(c0_mailbox1, 0x0);
+	c0_setval(c0_core_stat, 0x0);
+	c0_setbits(c0_core_reim, 0x3);
+	c0_setbits(c0_status, ST0_IM0 << IRQ_MBOX);
+	ehb();
 
 	// initialize the thread with the default stack size
 	thread_create(&idle_thread_core1, 0, NULL, NULL, NULL, 0);
