c77329a033bb0422dfb49f379fffade6464e9832
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-06 01:05:53 +0100

    JZ4780: Add defines for each irqchip (number of interrupts)

--- arch/mips/socs/jz4780/include/irq.h
+++ arch/mips/socs/jz4780/include/irq.h
@@ -2,6 +2,13 @@
 #define	_SOC_IRQ_H_
 
 
-#define	NR_IRQS	(8 + 64 + 6*32)
+#define	IRQ_CPU_NBR	8
+#define	IRQ_CPU_INTC		2
+#define	IRQ_CPU_MBOX		3
+#define	IRQ_CPU_OST		4
+
+#define	IRQ_INTC_NBR	64
+
+#define	IRQ_GPIO_NBR	(6*32)
 
 #endif
