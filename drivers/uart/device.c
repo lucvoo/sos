4db726d30c091c5cf00916c79b8d3f4dc9ed9e23
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-24 22:22:18 +0100

    UART: Add phony infrastructure

---
+++ drivers/uart/device.c
@@ -0,0 +1,8 @@
+#include "driver.h"
+
+
+int uart_register_dev(struct uart *p)
+{
+	// FIXME
+	return 0;
+}
---
+++ drivers/uart/driver.h
@@ -0,0 +1,8 @@
+#ifndef _UART_DRIVER_H_
+#define _UART_DRIVER_H_
+
+#include <uart.h>
+
+int uart_register_dev(struct uart *p);
+
+#endif
