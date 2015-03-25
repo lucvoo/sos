a888b5c6a9e8969c90736a039e02c4292e3a5942
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-25 22:11:39 +0100

    UART: Add the main definitions

---
+++ include/uart.h
@@ -0,0 +1,16 @@
+#ifndef	_UART_H_
+#define	_UART_H_
+
+struct uart;
+
+struct uart_ops {
+	int			(*start)(struct uart *);
+	int			(*stop)(struct uart *);
+};
+
+struct uart {
+	const char*		name;
+	struct uart_ops*	ops;
+};
+
+#endif
