cd4f846e401ec75ee31cb4fb53fda3ea1fbc8fdd
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-28 03:20:30 +0100

    JZ4780: Insure JTAG is enabled

--- arch/mips/socs/jz4780/init.c
+++ arch/mips/socs/jz4780/init.c
@@ -1,9 +1,21 @@
 #include <init.h>
 #include <iomap.h>
+#include <arch/memory.h>
+#include <io.h>
+#include <soc/baseaddr.h>
+#include <soc/gpio.h>
 
 
 static void jz4780_init(void)
 {
 	printf("JZ4780 booting ...\n");
+
+#ifdef	CONFIG_JTAG_ENABLE
+	// Enable JTAG/Disable UART3
+	iowrite32(KSEG1_BASE + GPIO_BASE + GPIO_INTC(0),  3 << 30);
+	iowrite32(KSEG1_BASE + GPIO_BASE + GPIO_MSKC(0),  3 << 30);
+	iowrite32(KSEG1_BASE + GPIO_BASE + GPIO_PAT1C(0), 3 << 30);
+	iowrite32(KSEG1_BASE + GPIO_BASE + GPIO_PAT0C(0), 3 << 30);
+#endif
 }
 core_initcall(jz4780_init);
