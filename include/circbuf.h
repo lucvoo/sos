16c5fc25ca52f95a6ccdc127e14bc37fc6f462d2
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-03-31 00:18:55 +0200

    CIRCBUF: Add iteration helpers

--- include/circbuf.h
+++ include/circbuf.h
@@ -23,4 +23,17 @@ static inline unsigned int circbuf_free(struct circbuf *buf, unsigned int order)
 	return (1 << order) - circbuf_busy(buf, order) - 1;
 }
 
+static inline unsigned int circbuf_inc_head(struct circbuf *buf, unsigned int order)
+{
+	return buf->head = (buf->head + 1) % (1 << order);
+}
+
+static inline unsigned int circbuf_inc_tail(struct circbuf *buf, unsigned int order)
+{
+	return buf->tail = (buf->tail + 1) % (1 << order);
+}
+
+#define	circbuf_iter_head(H, BUF, ORDER)	\
+	for (H = (BUF)->head; H != (BUF)->tail; H = (BUF)->head = ((H) + 1) % (1 << ORDER))
+
 #endif
