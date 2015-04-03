8d34c42c61d66dca3f0f32fa1f7de6dee12389ea
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-04-03 04:42:56 +0200

    NET: Real netdev_register() (and open the dev at the same time)

--- net/dev.c
+++ net/dev.c
@@ -1,9 +1,21 @@
 #include <net/dev.h>
+#include <printf.h>
+
+
+static int ndev_idx;
+
+static struct net_devices {
+	struct netdev*	next;
+} net_devices;
 
 
 int netdev_register(struct netdev *ndev)
 {
-	// FIXME
+	ndev->next = net_devices.next;
+	net_devices.next = ndev;
+
+	snprintf(ndev->ifname, sizeof(ndev->ifname), "eth%d", ndev_idx++);
+	ndev->open(ndev);
 
 	return 0;
 }
