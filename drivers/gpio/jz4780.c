c0fa42fd5a95ab62303d4e415fc14e1c1e5d87f5
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-04-07 00:50:57 +0200

    GPIO,JZ4780: Add GPIO support now

--- drivers/gpio/jz4780.c
+++ drivers/gpio/jz4780.c
@@ -1,9 +1,142 @@
+#include "gpiochip.h"
 #include <irqchip.h>
 #include <soc/gpio.h>
 #include <soc/baseaddr.h>
 #include <io.h>
 #include <irq.h>
 #include <init.h>
+
+
+static void jz4780_gpio_input(struct gpio *gpio)
+{
+	struct gpiochip *chip = gpio->chip;
+	void __iomem* iobase = chip->iobase;
+	int n = gpio->idx;
+
+	iowrite32(iobase + GPIO_PAT1S(0), 1 << n);
+}
+
+static void jz4780_gpio_set_value(struct gpio *gpio, int val)
+{
+	struct gpiochip *chip = gpio->chip;
+	void __iomem* iobase = chip->iobase;
+	unsigned long reg;
+	int n = gpio->idx;
+
+	if (val)
+		reg = GPIO_PAT1S(0);
+	else
+		reg = GPIO_PAT1C(0);
+	iowrite32(iobase + reg, 1 << n);
+}
+
+static void jz4780_gpio_output(struct gpio *gpio, int val)
+{
+	struct gpiochip *chip = gpio->chip;
+	void __iomem* iobase = chip->iobase;
+	int n = gpio->idx;
+
+	jz4780_gpio_set_value(gpio, val);
+	iowrite32(iobase + GPIO_PAT1C(0), 1 << n);
+}
+
+static int  jz4780_gpio_get_value(struct gpio *gpio)
+{
+	struct gpiochip *chip = gpio->chip;
+	void __iomem* iobase = chip->iobase;
+	int n = gpio->idx;
+
+	return (ioread32(iobase + GPIO_PIN(0)) >> n) & 0x1;
+}
+
+static void jz4780_gpio_set_irq(struct gpio *gpio, enum gpio_trigger_type type)
+{
+	struct gpiochip *chip = gpio->chip;
+	void __iomem* iobase = chip->iobase;
+	unsigned long pat1, pat0;
+	int n = gpio->idx;
+
+	switch (type) {
+	case GPIO_TRIG_LEVEL_LOW:
+		pat1 = GPIO_PAT1C(0);
+		pat0 = GPIO_PAT0C(0);
+		break;
+	case GPIO_TRIG_LEVEL_HIGH:
+		pat1 = GPIO_PAT1C(0);
+		pat0 = GPIO_PAT0S(0);
+		break;
+	case GPIO_TRIG_EDGE_FALL:
+		pat1 = GPIO_PAT1S(0);
+		pat0 = GPIO_PAT0C(0);
+		break;
+	case GPIO_TRIG_EDGE_RISE:
+		pat1 = GPIO_PAT1S(0);
+		pat0 = GPIO_PAT0S(0);
+		break;
+	default:
+		// FIXME
+		return;
+	}
+
+	iowrite32(iobase + GPIO_MSKS(0), 1 << n);
+	iowrite32(iobase + GPIO_INTS(0), 1 << n);
+	iowrite32(iobase + pat1, 1 << n);
+	iowrite32(iobase + pat0, 1 << n);
+}
+
+static void jz4780_gpio_alt_func(struct gpio *gpio, int func)
+{
+	struct gpiochip *chip = gpio->chip;
+	void __iomem* iobase = chip->iobase;
+	int n = gpio->idx;
+
+	switch (func) {
+	case -1: // GPIO input
+		iowrite32(iobase + GPIO_INTC(0), 1 << n);
+		iowrite32(iobase + GPIO_MSKC(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT1S(0), 1 << n);
+		break;
+	case 0:
+		iowrite32(iobase + GPIO_INTC(0), 1 << n);
+		iowrite32(iobase + GPIO_MSKC(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT1C(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT0C(0), 1 << n);
+		break;
+	case 1:
+		iowrite32(iobase + GPIO_INTC(0), 1 << n);
+		iowrite32(iobase + GPIO_MSKC(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT1C(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT0S(0), 1 << n);
+		break;
+	case 2:
+		iowrite32(iobase + GPIO_INTC(0), 1 << n);
+		iowrite32(iobase + GPIO_MSKC(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT1S(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT0C(0), 1 << n);
+		break;
+	case 3:
+		iowrite32(iobase + GPIO_INTC(0), 1 << n);
+		iowrite32(iobase + GPIO_MSKC(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT1S(0), 1 << n);
+		iowrite32(iobase + GPIO_PAT0S(0), 1 << n);
+		break;
+	default:
+		// FIXME
+		break;
+	}
+}
+
+
+static const struct gpio_ops jz4780_gpio_ops = {
+	.input =	jz4780_gpio_input,
+	.output =	jz4780_gpio_output,
+	.set_value =	jz4780_gpio_set_value,
+	.get_value =	jz4780_gpio_get_value,
+	.set_irq =	jz4780_gpio_set_irq,
+	.alt_func =	jz4780_gpio_alt_func,
+	.pull_up =	NULL,	// TODO
+	.pull_on =	NULL,	// TODO
+};
 
 
 static void jz4780_gpio_irq_ack(struct irqdesc *desc)
@@ -37,6 +170,8 @@ static void jz4780_gpio_irq_unmask(struct irqdesc *desc)
 
 
 struct jz4780_gpio {
+	struct gpiochip	gpiochip;
+	struct gpio	gpios[32];
 	struct irqchip	irqchip;
 	struct irqdesc	irqs[32];
 } jz4780_gpios[6];
@@ -69,6 +204,7 @@ static void jz4780_gpio_irq_handler(struct irqdesc *desc)
 static void jz4780_gpio_init_one(struct jz4780_gpio *chip, int port, const char *name)
 {
 	int parent_irqn = 17 - port;
+	struct gpiochip *gpiochip;
 	struct irqchip *irqchip;
 	struct irqdesc *parent;
 	void __iomem *iobase;
@@ -77,6 +213,16 @@ static void jz4780_gpio_init_one(struct jz4780_gpio *chip, int port, const char 
 	parent->handler = jz4780_gpio_irq_handler;
 
 	iobase = ioremap(GPIO_BASE + 0x100 * port, 0x100);
+
+	// pure GPIO part
+	gpiochip = &chip->gpiochip;
+	gpiochip->name = name;
+	gpiochip->nbr = 32;
+	gpiochip->iobase = iobase;
+	gpiochip->ops = &jz4780_gpio_ops;
+	gpiochip->gpios = chip->gpios;
+	gpiochip->irqs = chip->irqs;
+	gpiochip_register(gpiochip);
 
 	// IRQ part
 	irqchip = &chip->irqchip;
