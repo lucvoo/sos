4445878a5435e0899a2376468fecd7d929e3fea9
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-04-16 02:16:53 +0200

    MM: Add page_to_virt() & virt_to_page()

--- include/memory.h
+++ include/memory.h
@@ -10,4 +10,9 @@ extern struct page pagemap[];
 #define	pfn_to_page(pfn)	&pagemap[(pfn) - PFN_OFFSET]
 #define	page_to_pfn(pag)	(((pag) - pagemap) + PFN_OFFSET)
 
+#define	page_to_phys(pag)	__pfn_to_phys(page_to_pfn(pag))
+#define	page_to_virt(pag)	phys_to_virt(page_to_phys(pag))
+#define	phys_to_page(pad)	pfn_to_page(__phys_to_pfn(pad))
+#define	virt_to_page(ptr)	phys_to_page(virt_to_phys(ptr))
+
 #endif
