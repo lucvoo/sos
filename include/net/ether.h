3b6eb1351c62f7ad2d26678566a778b7a1b98760
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-04-16 05:06:40 +0200

    NET: Add support for eth_type()

--- include/net/ether.h
+++ include/net/ether.h
@@ -1,7 +1,11 @@
 #ifndef	_NET_ETHER_H_
 #define	_NET_ETHER_H_
 
+struct skb;
+
 
 void ether_dump_frame(const void *frame, unsigned int len);
+
+unsigned int eth_type(struct skb *skb);
 
 #endif
--- net/ether.c
+++ net/ether.c
@@ -1,5 +1,6 @@
 #include <net/if_ether.h>
 #include <net/ether.h>
+#include <net/skb.h>
 #include <byteorder.h>
 
 
@@ -12,4 +13,26 @@ void ether_dump_frame(const void *frame, unsigned int len)
 		"\tdst: %pM\n"
 		"\tsrc: %pM\n"
 		"\ttyp: %04x\n", len, len, hdr->dst, hdr->src, be16_to_cpu(hdr->proto));
+}
+
+
+unsigned int eth_type(struct skb *skb)
+{
+	const struct ethhdr *hdr;
+	unsigned int proto;
+
+	hdr = (const void *) skb->data;
+
+	// TODO: packet type
+
+	proto = be16_to_cpu(hdr->proto);
+	if (likely(proto >= ETH_P_802_3_MIN))
+		return proto;
+
+	// could this be an IPX packet on raw 802.3?
+	// (0xFFFF is an illegal DSAP/SSAP)
+	if (*((const u16*) &hdr[1]) == 0xFFFF)
+		return ETH_P_802_3;
+
+	return ETH_P_802_2;
 }
