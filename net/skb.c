fcf29f24a641204365fde19b0b0a1559f2a0edbc
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-05-05 02:53:24 +0200

    NET: Use slab for skb allocation

--- net/skb.c
+++ net/skb.c
@@ -1,20 +1,33 @@
 #include <net/skb.h>
 #include <page-alloc.h>
 #include <page.h>
+#include <slab.h>
 #include <memory.h>
 #include <round.h>
+#include <init.h>
 
 
-// FIXME: this should be changed to:
-//	1) allocate the skb from a specific kmem_cache
-//	2) we can allocate the buffer from a (big) block,
-//	   like from a stack and just increment an usage count.
-//	   To free it, just dec the usage count and free the block
-//	   when the usage count is zero.
+// FIXME: Instead of allocating a full page for each packet
+//	we should allocate the buffer from a (big) block,
+//	like from a stack and just increment an usage count.
+//	To free it, just dec the usage count and free the block
+//	when the usage count is zero.
+
+static struct kmem_cache *skb_cache;
+
+static void skb_cache_init(void)
+{
+	skb_cache = kmem_cache_create("skb", sizeof(struct skb), 0, 0);
+	if (!skb_cache) {
+		pr_err("failed to create skb_cache");
+	}
+}
+core_initcall(skb_cache_init);
+
+
 struct skb *skb_alloc(unsigned int len, unsigned int reserve)
 {
-	unsigned int skblen = ALIGN(sizeof(struct skb), 4);
-	unsigned int totlen = skblen + len + reserve;
+	unsigned int totlen = len + reserve;
 	struct skb *skb;
 	struct page *p;
 	void *buff;
@@ -22,14 +35,17 @@ struct skb *skb_alloc(unsigned int len, unsigned int reserve)
 	if (totlen > PAGE_SIZE)
 		return NULL;
 
+	skb = kmem_alloc(skb_cache);
+	if (!skb)
+		return NULL;
+
 	p = page_alloc(0, GFP_KERN);
 	if (!p) {
+		kmem_free(skb_cache, skb);
 		return NULL;
 	}
 	buff = page_to_virt(p);
 
-	skb  = buff;
-	buff += skblen;
 	skb->buff = buff;
 	skb->size = len + reserve;
 	skb->data = buff + reserve;
@@ -40,9 +56,10 @@ struct skb *skb_alloc(unsigned int len, unsigned int reserve)
 
 void skb_free(struct skb *skb)
 {
-	struct page *p = virt_to_page(skb);
+	struct page *p = virt_to_page(skb->buff);
 
 	page_free(p, 0);
+	kmem_free(skb_cache, skb);
 }
 
 
