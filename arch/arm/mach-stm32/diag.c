2b7ebd0c9fe259d3810bc4efce094d81cd1965f7
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-05-18 15:56:17 +0200

    STM32: Add early uart support

--- arch/arm/mach-stm32/diag.c
+++ arch/arm/mach-stm32/diag.c
@@ -1,7 +1,10 @@
 #include <diag.h>
 #include <init.h>
 #include <arch/io.h>
+#include <mach/baseaddr.h>
 #include <mach/usart.h>
+#include <mach/gpio.h>
+#include <mach/rcc.h>
 
 
 #define	USART_BASE	USART3_BASE
@@ -23,3 +26,49 @@ void _os_diag_write_char(unsigned int c)
 		stm32_putc(base, '\r');
 	stm32_putc(base, c);
 }
+
+
+static void iosl(void __iomem *addr, unsigned int val)
+{
+	iowrite32(addr, ioread32(addr) | val);
+}
+
+static void stm32_diag_init_gpio(void)
+{
+	// PB10 & PB11 => AF 7
+#define	PORT 1				// GPIO B
+
+	void __iomem *base = (void __iomem*) GPIO_BASE(PORT);
+	void __iomem *rbase = (void __iomem *) RCC_BASE;
+
+	// enable the clock
+	iosl(rbase + RCC_AHB1ENR, RCC_AHB1EN_GPIOEN(PORT));
+
+	iosl(base + GPIO_MODER, GPIO_MODE_AF(10)|GPIO_MODE_AF(11));
+	iosl(base + GPIO_OTYPER, GPIO_OTYPE_PP(10)|GPIO_OTYPE_PP(11));
+	iosl(base + GPIO_SPEEDR, GPIO_SPEED_FAST(10)|GPIO_SPEED_FAST(11));
+	iosl(base + GPIO_PUPDR, GPIO_PUPD_UP(10)|GPIO_PUPD_UP(11));
+	iosl(base + GPIO_AFR(10), GPIO_AF(10, 7)|GPIO_AF(11, 7));
+}
+
+static void __init stm32_diag_init(void)
+{
+	void __iomem *ubase = (void __iomem *) USART_BASE;
+	void __iomem *rbase = (void __iomem *) RCC_BASE;
+	unsigned int uartclk = 42000000;	// FIXME
+	unsigned int baud = 115200;
+	unsigned int div = (uartclk + baud/2) / baud;
+	unsigned int oversampling = 16;
+	unsigned int mant = div / oversampling;
+	unsigned int frac = div % oversampling;
+
+	stm32_diag_init_gpio();
+
+	// enable the clock
+	iosl(rbase + RCC_APB1ENR, RCC_APB1EN_USART3EN);
+
+	// configure the UART
+	iowrite32(ubase + USART_BRR, USART_BRR_M(mant) | USART_BRR_F(frac));
+	iosl(ubase + USART_CR1, USART_CR1_UE | USART_CR1_TE);
+}
+pure_initcall(stm32_diag_init);
--- arch/arm/mach-stm32/include/usart.h
+++ arch/arm/mach-stm32/include/usart.h
@@ -1,10 +1,84 @@
 #ifndef	_MACH_USART_H_
 #define	_MACH_USART_H_
 
-#define	USART3_BASE	0x40004800
+/*
+ * USART 1-3 & 6 are fully featured
+ * U/ART 4 & 5 are 'basic':
+ *	no HW flow control
+ *	no synchronous
+ *	no smartcard support
+ */
 
 #define	USART_SR	0x00
+#define	USART_SR_PE		(1 << 0)
+#define	USART_SR_FE		(1 << 1)
+#define	USART_SR_NF		(1 << 2)
+#define	USART_SR_ORE		(1 << 3)
+#define	USART_SR_IDLE		(1 << 4)
+#define	USART_SR_RXNE		(1 << 5)
+#define	USART_SR_TC		(1 << 6)
 #define	USART_SR_TXE		(1 << 7)
+#define	USART_SR_LBD		(1 << 8)
+#define	USART_SR_CTS		(1 << 9)
+#define	USART_SR_ERR_MASK	(USART_SR_LBD|USART_SR_ORE|USART_SR_FE|USART_SR_PE)
+#define	USART_SR_DUMMY_RX	(1 << 16)
+
 #define	USART_DR	0x04
+#define	USART_DR_MASK		0x00ff
+
+#define	USART_BRR	0x08
+#define	USART_BRR_F(X)		((X) << 0)
+#define	USART_BRR_M(X)		((X) << 4)
+
+#define	USART_CR1	0x0c
+#define	USART_CR1_SBK		(1 << 0)
+#define	USART_CR1_RWU		(1 << 1)
+#define	USART_CR1_RE		(1 << 2)
+#define	USART_CR1_TE		(1 << 3)
+#define	USART_CR1_IDLEIE	(1 << 4)
+#define	USART_CR1_RXNEIE	(1 << 5)
+#define	USART_CR1_TCIE		(1 << 6)
+#define	USART_CR1_TXEIE		(1 << 7)
+#define	USART_CR1_PEIE		(1 << 8)
+#define	USART_CR1_PS		(1 << 9)
+#define	USART_CR1_PCE		(1 << 10)
+#define	USART_CR1_WAKE		(1 << 11)
+#define	USART_CR1_M		(1 << 12)
+#define	USART_CR1_UE		(1 << 13)
+#define	USART_CR1_OVER8		(1 << 15)
+#define	USART_CR1_IE_MASK	0x01f0
+
+#define	USART_CR2	0x10
+#define	USART_CR2_ADDR_MASK	0x000f
+#define	USART_CR2_LBDL		(1 << 5)
+#define	USART_CR2_LBDIE		(1 << 6)
+#define	USART_CR2_LBCL		(1 << 8)
+#define	USART_CR2_CPHA		(1 << 9)
+#define	USART_CR2_CPOL		(1 << 10)
+#define	USART_CR2_CLKEN		(1 << 11)
+#define	USART_CR2_STOP_MASK	(3 << 12)
+#define	USART_CR2_STOP_1	(0 << 12)
+#define	USART_CR2_STOP_05	(1 << 12)
+#define	USART_CR2_STOP_2	(2 << 12)
+#define	USART_CR2_STOP_15	(3 << 12)
+#define	USART_CR2_LINEN		(1 << 14)
+
+#define	USART_CR3	0x14
+#define	USART_CR3_EIE		(1 << 0)
+#define	USART_CR3_IREN		(1 << 1)
+#define	USART_CR3_IRLP		(1 << 2)
+#define	USART_CR3_HDSEL		(1 << 3)
+#define	USART_CR3_NACK		(1 << 4)
+#define	USART_CR3_SCEN		(1 << 5)
+#define	USART_CR3_DMAR		(1 << 6)
+#define	USART_CR3_DMAT		(1 << 7)
+#define	USART_CR3_RTSE		(1 << 8)
+#define	USART_CR3_CTSE		(1 << 9)
+#define	USART_CR3_CTSIE		(1 << 10)
+#define	USART_CR3_ONEBIT	(1 << 11)
+
+#define	USART_GTPR	0x18
+#define	USART_GTPR_PSC_MASK	0x00ff
+#define	USART_GTPR_GT_MASK	0xff00
 
 #endif
