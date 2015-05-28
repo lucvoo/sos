831634a95133c9fe45cac792c90bfbf5e4f85189
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-05-28 16:56:01 +0200

    STM32: add a new set of clock parameters (this one for stm32f429-discovery)

--- arch/arm/soc-stm32/include/clock-config.h
+++ arch/arm/soc-stm32/include/clock-config.h
@@ -2,6 +2,7 @@
 #define	_SOC_CLOCK_H_
 
 
+#if	defined(CONFIG_STM32_CLK_12_168)
 #define HSE	12
 #define HCLK	168
 #define HPRE	RCC_CFGR_HPRE_1
@@ -12,5 +13,17 @@
 #define PLLP	4
 #define PLLQ	14
 #define WS	5
+#elif	defined(CONFIG_STM32_CLK_8_180)
+#define HSE	8
+#define HCLK	180
+#define HPRE	RCC_CFGR_HPRE_1
+#define PPRE1	RCC_CFGR_PPRE1_4
+#define PPRE2	RCC_CFGR_PPRE2_2
+#define PLLM	4
+#define PLLN	360
+#define PLLP	4
+#define PLLQ	15
+#define WS	5
+#endif
 
 #endif
