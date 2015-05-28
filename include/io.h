bcd269127976ae7d07b4f828343f2a96c773f8f0
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-05-28 16:43:17 +0200

    CORE: make ioremap() an no-op for NOMMU

--- include/io.h
+++ include/io.h
@@ -4,11 +4,22 @@
 #include <arch/io.h>
 #include <types/paddr_t.h>
 
+
 void __iomem *ioremap(paddr_t phys, unsigned long size);
 
 static inline void iounmap(void __iomem *addr, unsigned long size)
 {
 	// FIXME
 }
+
+
+#ifdef CONFIG_NOMMU
+static inline void __iomem *__ioremap_nop(paddr_t phys, unsigned long size)
+{
+	return (void __iomem *) phys;
+}
+
+#define	ioremap(A, S)	__ioremap_nop(A, S)
+#endif
 
 #endif
