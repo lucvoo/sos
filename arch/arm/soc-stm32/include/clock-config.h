470be5200c3e1170377b67a502ce1d9b897dd388
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-06-21 11:04:44 +0200

    STM32: Add the frequencies for the clock configs

--- arch/arm/soc-stm32/include/clock-config.h
+++ arch/arm/soc-stm32/include/clock-config.h
@@ -1,12 +1,16 @@
 #ifndef	_SOC_CLOCK_H_
 #define	_SOC_CLOCK_H_
 
+#include <soc/rcc.h>
 
 #if	defined(CONFIG_STM32_CLK_12_168)
 #define HSE	12
-#define HCLK	168
+#define SYSCLK	168
+#define	AHBDIV	1
 #define HPRE	RCC_CFGR_HPRE_1
+#define	APB1DIV	4
 #define PPRE1	RCC_CFGR_PPRE1_4
+#define	APB2DIV	2
 #define PPRE2	RCC_CFGR_PPRE2_2
 #define PLLM	6
 #define PLLN	336
@@ -15,9 +19,12 @@
 #define WS	5
 #elif	defined(CONFIG_STM32_CLK_8_180)
 #define HSE	8
-#define HCLK	180
+#define SYSCLK	180
+#define	AHBDIV	1
 #define HPRE	RCC_CFGR_HPRE_1
+#define	APB1DIV	4
 #define PPRE1	RCC_CFGR_PPRE1_4
+#define	APB2DIV	2
 #define PPRE2	RCC_CFGR_PPRE2_2
 #define PLLM	4
 #define PLLN	360
@@ -25,5 +32,12 @@
 #define PLLQ	15
 #define WS	5
 #endif
+
+#define	HCLK		(SYSCLK/AHBDIV)
+#define	APB1CLK		(HCLK/APB1DIV)
+#define	APB2CLK		(HCLK/APB2DIV)
+#define	TIM1CLK		(APB1CLK*2)
+#define	TIM2CLK		(APB2CLK*2)
+#define	SYSTICKCLK	(HCLK/8)
 
 #endif
