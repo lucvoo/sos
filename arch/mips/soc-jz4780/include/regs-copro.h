b91f25734527965521b8bb987d12ea153a4e3650
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-07-22 16:43:06 +0200

    JZ4780: add define and comment for short loops magic config7 bit

--- arch/mips/soc-jz4780/include/regs-copro.h
+++ arch/mips/soc-jz4780/include/regs-copro.h
@@ -27,6 +27,13 @@
 #define	CORE_REIM_IRQ1		(1 <<  9)
 #define	CORE_REIM_ENTRY_MSK	0xFFFF0000
 
+/*
+ * By default (when this bit is not set), the Xburst core will special casing
+ * short loops to avoid BTB lookup.
+ * This has as effect to vastly slow down tight, which we prefer to avoid!
+ */
+#define	CONFIG7_XBURST_SHORT_LOOPS_BTB_MAGIC	(1 << 4)
+
 #define	c0_mailbox0	__COPRO_REG($20, 0)
 #define	c0_mailbox1	__COPRO_REG($20, 1)
 
