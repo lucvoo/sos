a76ba44efe23f50b74f68491249df9cebc88ef7d
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-10-02 18:35:17 +0200

    CORE: add support for little-endian unaligned read

--- include/unaligned.h
+++ include/unaligned.h
@@ -2,5 +2,9 @@
 #define	_UNALIGNED_H_
 
 #include <generic/unaligned.h>
+#include <byteorder.h>
+
+#define	readle16(BUF)	le16_to_cpu(read16_unaligned(BUF))
+#define	readle32(BUF)	le32_to_cpu(read32_unaligned(BUF))
 
 #endif
