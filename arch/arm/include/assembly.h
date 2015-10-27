671f1417f88427dbacea4bf00e3611d5dec917c0
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-10-27 16:00:54 +0100

    ARM: define asm constraint to use for strex/ldrex

--- arch/arm/include/assembly.h
+++ arch/arm/include/assembly.h
@@ -1,11 +1,22 @@
 #ifndef	_ARCH_ASSEMBLY_H_
 #define	_ARCH_ASSEMBLY_H_
 
+#ifdef	__ASSEMBLY__
 	.syntax	unified
 
 #define	ASM_TYPE(type)	\
 	%type
 
 #include <generic/assembly.h>
+
+#else	// __ASSEMBLY__
+
+#ifdef	CONFIG_THUMB2
+#define	LLSC_MODE	"Qo"
+#else
+#define	LLSC_MODE	"Q"
+#endif
+
+#endif	// __ASSEMBLY__
 
 #endif
