878210befb273d91184cabeb67b6adb46ca45778
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-11-14 06:11:03 +0100

    ARM: put the whole mem range in the early MMU, not only the 4MB

--- arch/arm/include/memory.h
+++ arch/arm/include/memory.h
@@ -7,6 +7,9 @@
 #if defined(CONFIG_PHYS_ADDR) && !defined(PHYS_ADDR)
 #define	PHYS_ADDR	UL(CONFIG_PHYS_ADDR)
 #endif
+#if defined(CONFIG_PHYS_SIZE) && !defined(PHYS_SIZE)
+#define	PHYS_SIZE	UL(CONFIG_PHYS_SIZE)
+#endif
 
 #ifndef	VIRT_ADDR
 #define	VIRT_ADDR	UL(CONFIG_VIRT_ADDR)		// Virtual RAM. Same as Linux's PAGE_OFFSET
