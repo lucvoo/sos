0f07665980f7278653389f0583508c46419856a1
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-11-15 05:56:05 +0100

    ARM,IRQ,GIC: add support for the generic interrupt controller

---
+++ drivers/irq/arm-gic.c
@@ -0,0 +1,156 @@
+#include <irqchip.h>
+#include <irqdesc.h>
+#include <interrupt.h>
+#include <io.h>
+#include <init.h>
+#include <soc/irq.h>
+#include "arm-gic.h"
+
+
+struct gic_intctrl {
+	struct irqchip	chip;
+	struct irqdesc  descs[GIC_NBR_IRQ];
+	void __iomem	*cpu_base;
+};
+
+
+
+static void __iomem *gic_get_cpubase(struct gic_intctrl *gic)
+{
+	void __iomem *cpu_base = gic->cpu_base;
+
+	return cpu_base;
+}
+
+
+static void gic_irq_mask(struct irqdesc *desc)
+{
+	struct irqchip *chip = desc->chip;
+	uint irq = desc->irq;
+
+	iowrite32(chip->iobase + GICD_ICENABLER(irq/32), 1 << (irq % 32));
+}
+
+static void gic_irq_unmask(struct irqdesc *desc)
+{
+	struct irqchip *chip = desc->chip;
+	uint irq = desc->irq;
+
+	iowrite32(chip->iobase + GICD_ISENABLER(irq/32), 1 << (irq % 32));
+}
+
+static void gic_irq_eoi(struct irqdesc *desc)
+{
+	struct irqchip *chip = desc->chip;
+	struct gic_intctrl *gic = container_of(chip, struct gic_intctrl, chip);
+
+	iowrite32(gic_get_cpubase(gic) + GICC_EOIR, desc->irq);
+}
+
+
+static void gic_init_private(struct gic_intctrl *gic)
+{
+	void __iomem *dist_base = gic->chip.iobase;
+	void __iomem *cpu_base = gic_get_cpubase(gic);
+	uint cpu_ctrl;
+	int i;
+
+	// disable all PPIs & all SGIs
+	iowrite32(dist_base + GICD_ICENABLER(0), GIC_PPI_MASK|GIC_SGI_MASK);
+
+	// Set priority on PPI and SGI interrupts
+	for (i = 0; i < 32; i += 4)
+		iowrite32(dist_base + GICD_IPRIOR(i/4), GIC_PRIO_DEFAULT * 0x01010101U);
+
+	iowrite32(cpu_base + GICC_PMR, GIC_PRIO_FILTER);
+
+	// enable the CPU interface
+	cpu_ctrl = ioread32(cpu_base + GICC_CTLR);
+	cpu_ctrl |= GICC_CTLR_ENABLE;
+	iowrite32(cpu_base + GICC_CTLR, cpu_ctrl);
+}
+
+static void gic_init_shared(struct gic_intctrl *gic, uint irq_nbr)
+{
+	void __iomem *dist_base = gic->chip.iobase;
+	uint i;
+
+	iowrite32(dist_base + GICD_CTLR, GICD_CTLR_DISABLE);
+
+	for (i = 32; i < irq_nbr; i += 16)
+		iowrite32(dist_base + GICD_ICFGR(i/16), GICD_ICFGR_LEVEL * 0x55555555U);
+
+	for (i = 32; i < irq_nbr; i += 4)
+		iowrite32(dist_base + GICD_IPRIOR(i/4), GIC_PRIO_DEFAULT * 0x01010101U);
+
+	for (i = 32; i < irq_nbr; i += 32)
+		iowrite32(dist_base + GICD_ICENABLER(i/32), 0xFFFFFFFF);
+
+	for (i = 32; i < irq_nbr; i += 4)
+		iowrite32(dist_base + GICD_ITARGETSR(i/4), 0x01010101);
+
+	iowrite32(dist_base + GICD_CTLR, GICD_CTLR_ENABLE);
+}
+
+static struct gic_intctrl gic_intctrl;
+
+static void __init gic_init(void)
+{
+	struct gic_intctrl *gic = &gic_intctrl;
+	struct irqchip *chip = &gic->chip;
+	void __iomem *dist_base;
+	void __iomem *cpu_base;
+	uint irq_nbr;
+	uint lines;
+
+	cpu_base = ioremap(GICC_BASE, 0x0100);
+	dist_base = ioremap(GICD_BASE, 0x1000);
+
+	gic->cpu_base = cpu_base;
+	chip->iobase = dist_base;
+	chip->name = "gic";
+	chip->default_handler = irq_handle_eoi;
+	chip->mask	= gic_irq_mask;
+	chip->unmask	= gic_irq_unmask;
+	chip->ack	= gic_irq_eoi;
+
+	lines = ioread32(chip->iobase + GICD_TYPER);
+	lines = GICD_TYPER_LINES(lines);
+	irq_nbr = (lines + 1) * 32;
+	if (irq_nbr > GIC_NBR_IRQ)
+		irq_nbr = GIC_NBR_IRQ;
+	chip->irq_nbr = GIC_NBR_IRQ;
+
+	gic_init_shared(gic, irq_nbr);
+	gic_init_private(gic);
+
+	irqchip_init(NULL, chip);
+}
+board_irq_initcall(gic_init);
+
+
+/******************************************************************************/
+#include <symbols.h>
+
+static void gic_handle_irq(struct eframe *regs)
+{
+	struct gic_intctrl *gic = &gic_intctrl;
+	struct irqchip *chip = &gic->chip;
+	void __iomem *cpu_base = gic_get_cpubase(gic);
+
+//printf("%s(): reached!\n", __func__);
+	while (1) {
+		u32 iar = ioread32(cpu_base + GICC_IAR);
+		u32 irq = GICC_IAR_INTID(iar);
+		struct irqdesc *desc = &chip->descs[irq];
+//printf("%s(): irq = %d\n", __func__, irq);
+
+		if (likely(irq >= 16 && irq < GIC_MAX_IRQ)) {
+			__irq_handler(desc, regs);
+		} else if (irq < 16) {
+			iowrite32(cpu_base + GICC_EOIR, iar);
+		} else
+			return;
+	}
+}
+strong_alias(gic_handle_irq, mach_irq_handler);
---
+++ drivers/irq/arm-gic.h
@@ -0,0 +1,64 @@
+#ifndef _ARM_GIC_H_
+#define _ARM_GIC_H_
+
+
+// Distributor
+#define GICD_CTLR		 0x0000
+#define GICD_CTLR_ENABLE		0x1
+#define GICD_CTLR_DISABLE		0x0
+#define GICD_TYPER		 0x0004
+#define	GICD_TYPER_LINES(VAL)		((VAL) & 0x1f)
+#define GICD_IIDR		 0x0008
+#define GICD_IGROUP(N)		(0x0080 + (N) * 4)
+#define GICD_ISENABLER(N)	(0x0100 + (N) * 4)
+#define GICD_ICENABLER(N)	(0x0180 + (N) * 4)
+#define GICD_ISPENDINGR(N)	(0x0200 + (N) * 4)
+#define GICD_ICPENDINGR(N)	(0x0280 + (N) * 4)
+#define GICD_ISACTIVER(N)	(0x0300 + (N) * 4)
+#define GICD_ICACTIVER(N)	(0x0380 + (N) * 4)
+#define GICD_IPRIOR(N)		(0x0400 + (N) * 4)
+#define GICD_ITARGETSR(N)	(0x0800 + (N) * 4)
+#define GICD_ICFGR(N)		(0x0c00 + (N) * 4)
+#define GICD_ICFGR_LEVEL		0b00
+#define GICD_ICFGR_EDGE			0b01
+#define GICD_ICFGR_MASK			0b11
+#define GICD_NSACR(N)		(0x0e00 + (N) * 4)
+#define GICD_SGIR		 0x0f00
+#define GICD_CPENDSGIR(N)	(0x0f10 + (N) * 4)
+#define GICD_SPENDSGIR(N)	(0x0f20 + (N) * 4)
+
+//#define GICD_INT_ACTLOW_LVLTRIG		0x0
+
+
+// CPU
+#define GICC_CTLR		 0x0000
+#define GICC_CTLR_ENABLE		0x1
+#define GICC_PMR		 0x0004
+#define GICC_BPR		 0x0008
+#define GICC_IAR		 0x000c
+#define GICC_IAR_INTID(IAR)		((IAR) & 0x3ff)
+#define GICC_IAR_CPUID(IAR)		(((IAR) >> 10 ) & 0x7)
+#define GICC_EOIR		 0x0010
+#define GICC_RPR		 0x0014
+#define GICC_HPPIR		 0x0018
+#define GICC_ABPR		 0x001C
+#define GICC_AIAR		 0x0020
+#define GICC_AEOIR		 0x0024
+#define GICC_AHPPIR		 0x0028
+#define GICC_APR(N)		(0x00d0 + (N) * 4)
+#define GICC_NSAPR(N)		(0x00e0 + (N) * 4)
+#define GICC_IIDR		 0x00fc
+#define GICC_DIR		 0x1000
+
+
+#define	GIC_PPI_MASK			0xffff0000
+#define	GIC_SGI_MASK			0x0000ffff
+
+#define GIC_PRIO_DEFAULT		0xc0
+#define GIC_PRIO_FILTER			0xf0
+
+#define	GIC_MAX_CPU			8
+
+#define	GIC_MAX_IRQ			1020
+
+#endif
