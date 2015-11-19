33f31b71feb4073d942eaef8ade9d6d8fd8e15ea
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-11-19 04:47:25 +0100

    IMX6: add GPT params

---
+++ arch/arm/soc-imx6/include/baseaddr.h
@@ -0,0 +1,7 @@
+#ifndef	_SOC_BASEADDR_H_
+#define	_SOC_BASEADDR_H_
+
+
+#define	GPT_BASE	0x02098000
+
+#endif
---
+++ arch/arm/soc-imx6/include/irqs.h
@@ -0,0 +1,7 @@
+#ifndef	_SOC_IRQS_H_
+#define	_SOC_IRQS_H_
+
+
+#define	IRQ_GPT		87
+
+#endif
