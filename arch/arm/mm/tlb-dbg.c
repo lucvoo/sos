627a0ba965f10a0cb079e853e3736cbe2678d770
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2015-11-22 14:19:33 +0100

    ARM,MMU: early debugging of the MMU table

---
+++ arch/arm/mm/tlb-dbg.c
@@ -0,0 +1,37 @@
+#include <diag.h>
+#include <stringify.h>
+#include <arch/cp15.h>
+
+
+static void dump_tbl(const u32 *tbl, uint idx)
+{
+	int i;
+
+	_os_diag_write_char('T');
+	_os_diag_write_char(idx);
+	_os_diag_write_char(':');
+	_os_diag_write_char('\n');
+	for (i = 0; i < 4096; i ++) {
+		_os_diag_hexval(i);
+		_os_diag_write_char(':');
+		_os_diag_hexval(tbl[i]);
+		_os_diag_write_char('\n');
+	}
+}
+
+void early_tlb_dump(void);
+void early_tlb_dump(void)
+{
+	u32 ttbcr;
+	u32 *tbl;
+
+	asm volatile ("mrc " STRINGIFY(TTBCR(%0)): "=r" (ttbcr) :: "memory");
+	_os_diag_write_char('T');
+	_os_diag_write_char('C');
+	_os_diag_write_char(':');
+	_os_diag_hexval(ttbcr);
+	_os_diag_write_char('\n');
+
+	asm volatile ("mrc " STRINGIFY(TTBR0(%0)): "=r" (tbl) :: "memory");
+	dump_tbl(tbl, '0');
+}
