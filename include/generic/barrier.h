5a32be89a999fc63d98a412bdf65e4b4ec4d6e1b
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-03-16 22:55:14 +0100

    SYNC: Add generic SMP memory barriers

--- include/generic/barrier.h
+++ include/generic/barrier.h
@@ -19,4 +19,35 @@
 #define	mbw()	mb()
 #endif
 
+
+#ifndef	CONFIG_SMP
+
+#ifndef	mb_smp
+#define	mb_smp()	barrier()
+#endif
+
+#ifndef	mbr_smp
+#define	mbr_smp()	barrier()
+#endif
+
+#ifndef	mbw_smp
+#define	mbw_sm()	barrier()
+#endif
+
+#else	// CONFIG_SMP
+
+#ifndef	mb_smp
+#define	mb_smp()	mb()
+#endif
+
+#ifndef	mbr_smp
+#define	mbr_smp()	mbr()
+#endif
+
+#ifndef	mbw_smp
+#define	mbw_smp()	mbw()
+#endif
+
+#endif	// CONFIG_SMP
+
 #endif
