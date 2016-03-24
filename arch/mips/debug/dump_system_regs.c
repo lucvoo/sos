c391ab917a19e509811c3c9304e3ab710289b14e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-03-24 12:02:17 +0100

    MIPS: extend dump_system_regs() with c0_intctl & c0_srsctl

--- arch/mips/debug/dump_system_regs.c
+++ arch/mips/debug/dump_system_regs.c
@@ -31,6 +31,8 @@ void dump_system_regs(void)
 	dump_cp0(c0_hwrena);
 	dump_cp0(c0_count);
 	dump_cp0(c0_prid);
+	dump_cp0(c0_intctl);
+	dump_cp0(c0_srsctl);
 	dump_cp0(c0_ebase);
 	dump_cp0(c0_config);
 	dump_cp0(c0_config1);
