197256db0bbcb5730650e4c5118e5a22f1800f21
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-03-24 14:29:42 +0100

    AR9331: add CPU parameters (cache organization, TLB, ...)

---
+++ arch/mips/soc-ar933x/include/cache.h
@@ -0,0 +1,12 @@
+#ifndef	_SOC_CACHE_H_
+#define	_SOC_CACHE_H_
+
+#define	CACHE_LI_SETS		512
+#define	CACHE_LI_WAYS		4
+#define	CACHE_LI_LINESIZE	32
+
+#define	CACHE_LD_SETS		256
+#define	CACHE_LD_WAYS		4
+#define	CACHE_LD_LINESIZE	32
+
+#endif
---
+++ arch/mips/soc-ar933x/include/cpu-config.h
@@ -0,0 +1,11 @@
+#ifndef	_SOC_CPU_CONFIG_H_
+#define	_SOC_CPU_CONFIG_H_
+
+// TLB
+#define	TLB_ENTRIES	16
+
+// Misc
+#define	IRQ_TI		7	// IP7 -> timer/counter interrupt
+#define	IRQ_PC		6	// IP6 -> performance counter interrupt
+
+#endif
