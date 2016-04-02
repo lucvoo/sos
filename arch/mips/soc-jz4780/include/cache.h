1f2c250e191cbd8a90a9005b815fc4fd1b206c42
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-04-02 17:55:40 +0200

    JZ4780: add cache parameters

---
+++ arch/mips/soc-jz4780/include/cache.h
@@ -0,0 +1,12 @@
+#ifndef	_SOC_CACHE_H_
+#define	_SOC_CACHE_H_
+
+#define	CACHE_LI_SETS		128
+#define	CACHE_LI_WAYS		8
+#define	CACHE_LI_LINESIZE	32
+
+#define	CACHE_LD_SETS		128
+#define	CACHE_LD_WAYS		8
+#define	CACHE_LD_LINESIZE	32
+
+#endif
