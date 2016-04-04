06aa0d13b16cc013a5fe393e175b2ca3adf44f3e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-04-04 02:22:27 +0200

    OMEGA: add <soc/irqs.h>

---
+++ arch/mips/soc-ar933x/include/irqs.h
@@ -0,0 +1,25 @@
+#ifndef	_SOC_IRQS_H_
+#define	_SOC_IRQS_H_
+
+
+#define	IRQ_CPU_WMAC	2
+#define	IRQ_CPU_USB	3
+#define	IRQ_CPU_ETH0	4
+#define	IRQ_CPU_ETH1	5
+#define	IRQ_CPU_MISC	6
+#define	IRQ_CPU_TIMER	7
+
+#define IRQ_MISC_ETHSW	12
+#define IRQ_MISC_TIMER4	10
+#define IRQ_MISC_TIMER3	9
+#define IRQ_MISC_TIMER2	8
+#define IRQ_MISC_DMA	7
+#define IRQ_MISC_OHCI	6
+#define IRQ_MISC_PERFC	5
+#define IRQ_MISC_WDOG	4
+#define IRQ_MISC_UART	3
+#define IRQ_MISC_GPIO	2
+#define IRQ_MISC_ERROR	1
+#define IRQ_MISC_TIMER0	0
+
+#endif
