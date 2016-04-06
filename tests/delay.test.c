8dfbd44ab6c2a5168863321dc97e80330dbe8106
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-04-06 22:54:17 +0200

    TEST: add test for udelay()

---
+++ tests/delay.test.c
@@ -0,0 +1,45 @@
+#include <kapi/kapi.h>
+#include <version.h>
+#include <utils.h>
+#include <timer.h>
+#include <delay.h>
+#include <timerdev.h>
+
+
+static struct timer timer;
+static volatile int next;
+
+static void timer_action(void *data)
+{
+	next = 1;
+}
+
+void kapi_start(void)
+{
+	ulong t0;
+
+	printf(os_version);
+
+	timer.exp = 10 * HZ;
+	timer.action = timer_action;
+	timer.data = &t0;
+
+	while (1) {
+		int n;
+
+		next = 0;
+		t0 = timerdev_read();
+
+#define	SECS	10
+		timer.exp = SECS * HZ;
+		timer_add_rel(&timer);
+
+		n = 0;
+#define	STEPS 1000
+		do {
+			udelay(STEPS);
+			n++;
+		} while (next == 0);
+		printf("n = %d (must be ~ %d)\n", n * STEPS, SECS * 1000000);
+	}
+}
