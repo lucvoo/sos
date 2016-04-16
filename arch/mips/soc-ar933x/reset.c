f3f4dd9838d6081f5a240ec4d531c404d0275d88
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-04-16 22:10:32 +0200

    AR9331: improve tracability of ath79_reset_set() & ath79_reset_clr()

--- arch/mips/soc-ar933x/reset.c
+++ arch/mips/soc-ar933x/reset.c
@@ -15,9 +15,12 @@ void ath79_reset_set(u32 mask)
 {
 	void __iomem *ioaddr = reset_iobase + AR933X_RESET_REG_RESET;
 	ulong flags;
+	u32 old, new;
 
 	flags = lock_acq_save(&reset_lock);
-	iowrite32(ioaddr, ioread32(ioaddr) | mask);
+	old = ioread32(ioaddr);
+	new = old | mask;
+	iowrite32(ioaddr, new);
 	lock_rel_rest(&reset_lock, flags);
 }
 
@@ -25,8 +28,11 @@ void ath79_reset_clr(u32 mask)
 {
 	void __iomem *ioaddr = reset_iobase + AR933X_RESET_REG_RESET;
 	ulong flags;
+	u32 old, new;
 
 	flags = lock_acq_save(&reset_lock);
-	iowrite32(ioaddr, ioread32(ioaddr) & ~mask);
+	old = ioread32(ioaddr);
+	new = old & ~mask;
+	iowrite32(ioaddr, new);
 	lock_rel_rest(&reset_lock, flags);
 }
