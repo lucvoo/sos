ac9783deede9de0a8d84bc8f28a09236c870356a
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-12 10:36:56 +0200

    ARM,SMP: add __coreid()

---
+++ arch/arm/include/smp.h
@@ -0,0 +1,17 @@
+#ifndef	_ARCH_SMP_H_
+#define	_ARCH_SMP_H_
+
+#include <arch/cp15.h>
+#include <soc/smp.h>
+
+
+static inline uint __coreid(void)
+{
+	u32 mpidr = cp_read(MPIDR);
+	uint cluster = MPIDR_CLUSTER(mpidr);
+	uint core = MPIDR_CORE(mpidr);
+
+	return (cluster * 4) | core;
+}
+
+#endif
