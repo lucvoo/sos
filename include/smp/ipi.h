51266ae5e5dd309d5ff32fcff1b946f861e72d79
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-14 22:44:02 +0200

    SMP: add defines for IPI destinations: ALL, OTHER & SELF

--- include/smp/ipi.h
+++ include/smp/ipi.h
@@ -2,6 +2,10 @@
 #define _SMP_IPI_H_
 
 
+#define	SMP_IPI_ALL	-1
+#define	SMP_IPI_OTHER	-2
+#define	SMP_IPI_SELF	-3
+
 #define	SMP_IPI_PING		0x00000001
 #define	SMP_IPI_PONG		0x00000002
 #define	SMP_IPI_SCHEDULE	0x00000004
