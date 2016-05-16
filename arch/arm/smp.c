6a87c12fd678075a066afe4823868e8f80e9f61b
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-17 01:52:18 +0200

    ARM,SMP: add SMP support

---
+++ arch/arm/smp.c
@@ -0,0 +1,57 @@
+#include <smp.h>
+#include <smp/init.h>
+#include <smp/ops.h>
+#include <thread.h>
+
+
+#define	foreach_cpu(cpu)	for (cpu = 0; cpu < NR_CPUS; cpu++)
+
+// FIXME: should allocate this dynamically
+static struct thread idle_threads[NR_CPUS-1];
+
+static int smp_boot_cpu(uint cpu)
+{
+	struct thread *idle = &idle_threads[cpu-1];
+	int rc;
+
+	// initialize the thread with the default stack size
+	thread_create(idle, 0, NULL, NULL, NULL, 0);
+
+
+	rc = smp_ops.boot_cpu(idle, cpu);
+	return rc;
+}
+
+int __smp_init(void)
+{
+	uint cpu;
+	int rc;
+
+
+	// do any global initialization
+
+
+	foreach_cpu(cpu) {
+		if (cpu == 0)
+			continue;
+
+		// allocate thread & stack
+
+
+		// boot the cpu
+		// this will call smp_entry()
+		// which will do:
+		//	* some generic inits
+		//	* call smp_secondary_init() for specific inits
+		//	* call smp_start()
+		rc = smp_boot_cpu(cpu);
+		if (rc) {
+			pr_err("cpu %u: booting returns %d\n", cpu, rc);
+			continue;
+		}
+
+		// FIXME: should wait till ??? is finish/released?
+	}
+
+	return 0;
+}
