58d6405297556aa4b43a80fedd478877e75eb77b
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-17 18:06:35 +0200

    SMP: need to call smp_ops.init_cpu() too

--- kernel/smp/init.c
+++ kernel/smp/init.c
@@ -1,5 +1,7 @@
 #include <kapi/kapi.h>
 #include <smp/init.h>
+#include <smp/ops.h>
+#include <smp.h>
 #include <symbols.h>
 #include <idle.h>
 #include <sched.h>
@@ -7,6 +9,10 @@
 
 void __smp_start(void)
 {
+	uint cpu = __coreid();
+
+	smp_ops.init_cpu(cpu);
+
 	kapi_start_smp();
 	_thread_scheduler_start();
 	__cpu_idle();
