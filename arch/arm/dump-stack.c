3c82806e36b247a014db023a07db16dd42685142
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-17 17:10:31 +0200

    ARM,DBG: use printf()'s locking for dump_stack()

--- arch/arm/dump-stack.c
+++ arch/arm/dump-stack.c
@@ -1,11 +1,18 @@
 #include <exceptions.h>
 #include <arch/asm-offsets.h>
+#include <lock.h>
 
 
 void dump_stack(const struct eframe *f, unsigned int flags)
 {
+	extern struct lock printf_lock;
+	ulong status = lock_acq_save(&printf_lock);
+#define	printf	__printf
+
 	printf("PC  %08x, LR  %08x, SP  %08x, FP  %08x, PSR %08x\n", f->pc, f->lr, f->sp, f->r[11], f->cpsr);
 	printf("R0  %08x, R1  %08x, R2  %08x, R3  %08x\n", f->r[0], f->r[1], f->r[2], f->r[3]);
 	printf("R4  %08x, R5  %08x, R6  %08x, R7  %08x\n", f->r[4], f->r[5], f->r[6], f->r[7]);
 	printf("R8  %08x, R9  %08x, R10 %08x, R12 %08x\n", f->r[8], f->r[9], f->r[10], f->r[12]);
+
+	lock_rel_rest(&printf_lock, status);
 }
