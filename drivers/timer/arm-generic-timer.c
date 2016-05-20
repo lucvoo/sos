9b256e246d50038d0b352bb09f7d02fa21a0e276
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-21 01:57:20 +0200

    ARM: add support for ARM's architectural/generic timer

---
+++ drivers/timer/arm-generic-timer.c
@@ -0,0 +1,147 @@
+#include <timerdev.h>
+#include <soc/generic-timer.h>
+#include <arch/barrier.h>
+#include <interrupt.h>
+#include <arch/cp15.h>
+#include <errno.h>
+#include <init.h>
+#include <irq.h>
+
+
+// FIXME: for now we only can use the virt timer
+//	  we may have reasons to use another one
+
+
+static inline u64 agt_counter(void)
+{
+	u64 val;
+
+	isb();
+	val = cp_read64(CNTVCNT);
+	return val;
+}
+
+
+static int agt_isr(struct irqdesc *desc, void *data)
+{
+	u32 ctrl;
+
+	ctrl = cp_read(CNTV_CTL);
+	if (!(ctrl & CNTX_CTL_ISTATUS))
+		return IRQ_NONE;
+
+	ctrl |= CNTX_CTL_IMASK;
+	cp_write(CNTV_CTL, ctrl);
+
+	return IRQ_HANDLED | IRQ_CALL_DSR;
+}
+
+static int agt_dsr(struct irqdesc *desc, unsigned int count, void *data)
+{
+	struct timerdev *td = data;
+
+	td->handler(td);
+	return 0;
+}
+
+// FIXME: - 32bit but HW allow 64bit
+//	  - freq is quite high (20-50MHz)
+//	    => rollover < 3 minutes
+static ulong agt_now(struct timerdev *td)
+{
+	ulong now;
+
+	now = agt_counter();
+	return now;
+}
+
+static int agt_next_rel(struct timerdev *td, ulong val)
+{
+	u32 ctrl;
+
+	// FIXME: overrun is quite short (3 min)
+
+	ctrl = cp_read(CNTV_CTL);
+	ctrl |= CNTX_CTL_ENABLE;
+	ctrl &= ~CNTX_CTL_IMASK;
+	cp_write(CNTV_TVAL, val);
+	cp_write(CNTV_CTL, ctrl);
+
+	return 0;
+}
+
+static int agt_stop(struct timerdev *td)
+{
+	u32 ctrl;
+
+	ctrl = cp_read(CNTV_CTL);
+	ctrl &= ~CNTX_CTL_ENABLE;
+	cp_write(CNTV_CTL, ctrl);
+
+	return 0;
+}
+
+
+static void agt_user_access(void)
+{
+	u32 ctrl = cp_read(CNTKCTL);
+
+	// only allow access to the virtual counter
+	ctrl &= CNTKCTL_PRESERVE;
+	ctrl |= CNTKCTL_PL0VCTEN;
+
+	cp_write(CNTKCTL, ctrl);
+}
+
+static int agt_init_local(struct timerdev *td, uint cpu)
+{
+	struct irqdesc *desc = irq_get_desc("gic", TIMER_IRQ);
+
+	agt_user_access();
+	irq_unmask(desc);
+
+	return 0;
+}
+
+static void agt_stop_local(struct timerdev *td)
+{
+	struct irqdesc *desc = irq_get_desc("gic", TIMER_IRQ);
+
+	irq_mask(desc);
+}
+
+
+static struct timerdev arm_generic_timer;
+static struct irqaction agt_irq;
+
+static int __init agt_init(void)
+{
+	struct timerdev *td = &arm_generic_timer;
+	struct irqdesc *desc;
+	int rc = EINVAL;
+
+	desc = irq_get_desc("gic", TIMER_IRQ);
+	if (!desc)
+		goto err_irqdesc;
+
+	td->name = "generic_timer";
+	td->now      = agt_now;
+	td->next_rel = agt_next_rel;
+
+	irq_create(&agt_irq, agt_isr, agt_dsr, td, 0);
+	irq_attach(desc, &agt_irq);
+
+	agt_init_local(td, 0);
+
+	rc = timerdev_register(td);
+	return rc;
+
+err_irqdesc:
+	return rc;
+}
+
+static void __init arm_generic_timer_init(void)
+{
+	agt_init();
+}
+board_initcall(arm_generic_timer_init);
