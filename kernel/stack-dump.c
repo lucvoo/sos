6c4431c7c15e1a0ce3a7edad42022b99c957d761
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-05-31 17:25:30 +0200

    CORE: add a header to stack dumps

--- kernel/stack-dump.c
+++ kernel/stack-dump.c
@@ -7,6 +7,9 @@ void dump_stack(const struct eframe *f, uint flags)
 	extern struct lock printf_lock;
 	ulong status = lock_acq_save(&printf_lock);
 
+	__printf("\nSTACK DUMP:");
+	__printf("\n");
+
 	__arch_dump_stack(f);
 
 	lock_rel_rest(&printf_lock, status);
