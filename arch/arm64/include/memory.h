1596ce1850e541f3e5428a0a6219cb966d4ce40e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-06-10 11:47:48 +0200

    ARM64: add defines for address space

--- arch/arm64/include/memory.h
+++ arch/arm64/include/memory.h
@@ -2,18 +2,25 @@
 #define	_ARCH_MEMORY_H_
 
 #include <asm-ul.h>
+#include <arch/page.h>
+#include <soc/memory.h>
 
 
-#if defined(CONFIG_PHYS_ADDR) && !defined(PHYS_ADDR)
+#define	VIRT_BITS	CONFIG_VA_BITS
+#define	VIRT_LAST	UL(0xffffffffffffffff)
+
 #define	PHYS_ADDR	UL(CONFIG_PHYS_ADDR)
-#endif
+#define	VIRT_ADDR	(UL(0xffffffffffffffff) << VIRT_BITS)
 
-#ifndef	VIRT_ADDR
-#define	VIRT_ADDR	UL(CONFIG_VIRT_ADDR)		// Virtual RAM. Same as Linux's PAGE_OFFSET
-#endif
 #ifndef	TEXT_OFFSET
 #define	TEXT_OFFSET	UL(CONFIG_TEXT_OFFSET)
 #endif
+
+
+// TOP IOMEM_SIZE
+#define	IOMEM_VIRT	(VIRT_LAST - IOMEM_SIZE + 1)
+
+// TODO: reserve space for VMALLOC, PCI IO, ...
 
 
 #ifndef	__ASSEMBLY__
