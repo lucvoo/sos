1beee0abb5917a6c84a36154f9b4b234cda9200b
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-06-13 12:43:35 +0200

    ARM64: dump the regs during an exception

--- arch/arm64/exception.c
+++ arch/arm64/exception.c
@@ -1,4 +1,5 @@
 #include <exceptions.h>
+#include <halt.h>
 
 
 // called from vector.S
@@ -6,5 +7,13 @@ void __sync_handler(struct eframe *regs, ulong far, u32 esr);
 
 void __sync_handler(struct eframe *regs, ulong far, u32 esr)
 {
-	// TODO
+	uint ec = (esr >> 26);
+	uint il = (esr >> 25) & 0x1;
+	uint iss = esr & 0x1FFFFFF;
+
+
+	printf("\nException:\nfar %016lx esr %08x "
+			"(%06b %01b 0x%07x)\n", far, esr, ec, il, iss);
+
+	__halt(regs);
 }
