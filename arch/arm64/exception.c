df013b1b016b88b31fa8acfbb7c77f6187632b53
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-06-13 14:33:53 +0200

    ARM64: add BUG() support

--- arch/arm64/exception.c
+++ arch/arm64/exception.c
@@ -1,5 +1,23 @@
 #include <exceptions.h>
+#include <kernel/bug.h>
+#include <arch/esr.h>
 #include <halt.h>
+#include <trap.h>
+
+
+static void handle_brk(struct eframe *regs, ulong far, u32 esr)
+{
+	uint val = esr & 0xFFFF;
+
+	switch (val) {
+	case TRAP_BUG:
+		__bug(regs, NULL, NULL, 0);
+		break;
+
+	default:
+		und_handler(regs);
+	}
+}
 
 
 // called from vector.S
@@ -11,6 +29,10 @@ void __sync_handler(struct eframe *regs, ulong far, u32 esr)
 	uint il = (esr >> 25) & 0x1;
 	uint iss = esr & 0x1FFFFFF;
 
+	switch (ec) {
+	case ESR_EC_BRK64:
+		return handle_brk(regs, far, esr);
+	}
 
 	printf("\nException:\nfar %016lx esr %08x "
 			"(%06b %01b 0x%07x)\n", far, esr, ec, il, iss);
---
+++ arch/arm64/include/bug.h
@@ -0,0 +1,6 @@
+#ifndef	_ARCH_BUG_H_
+#define	_ARCH_BUG_H_
+
+#include <generic/bug-trap.h>
+
+#endif
