2c0e2ccdc3d357ae567740e0f50eb07f7e3eea49
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-06-13 15:08:42 +0200

    ARM64: display the name of (some) fatal exceptions

--- arch/arm64/exception.c
+++ arch/arm64/exception.c
@@ -20,6 +20,12 @@ static void handle_brk(struct eframe *regs, ulong far, u32 esr)
 }
 
 
+static const char *exception_names[] = {
+	[ESR_EC_PC_ALIGN] = "PC alignment fault",
+	[ESR_EC_SP_ALIGN] = "SP alignment fault",
+	[ESR_EC_UNKNOWN] = "unknown",
+};
+
 // called from vector.S
 void __sync_handler(struct eframe *regs, ulong far, u32 esr);
 
@@ -28,14 +34,18 @@ void __sync_handler(struct eframe *regs, ulong far, u32 esr)
 	uint ec = (esr >> 26);
 	uint il = (esr >> 25) & 0x1;
 	uint iss = esr & 0x1FFFFFF;
+	const char *name;
 
 	switch (ec) {
 	case ESR_EC_BRK64:
 		return handle_brk(regs, far, esr);
+	default:
+		break;
 	}
 
-	printf("\nException:\nfar %016lx esr %08x "
-			"(%06b %01b 0x%07x)\n", far, esr, ec, il, iss);
+	name = exception_names[ec] ? : "???";
+	printf("\nException: %s\nfar %016lx esr %08x "
+			"(%06b %01b 0x%07x)\n", name, far, esr, ec, il, iss);
 
 	__halt(regs);
 }
