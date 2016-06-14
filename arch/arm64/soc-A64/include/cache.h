551b37308d037990b2605ae6c16db48dfb0fa34c
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-06-14 02:47:56 +0200

    A64: add defines for cache parameters

---
+++ arch/arm64/soc-A64/include/cache.h
@@ -0,0 +1,21 @@
+#ifndef	_SOC_CACHE_H_
+#define	_SOC_CACHE_H_
+
+#define	CACHE_LI_SETS		128
+#define	CACHE_LI_WAYS		4
+#define	CACHE_LI_LINESIZE	64
+
+#define	CACHE_LD_SETS		128
+#define	CACHE_LD_WAYS		4
+#define	CACHE_LD_LINESIZE	64
+
+
+// LoUIS: L2
+// LoC:	  L2
+// LoUU:  L2
+
+// Note: L2 must be used dynamically since
+//	 it's not the same on the 2 clusters:
+//	 512K / 2048 K
+
+#endif
