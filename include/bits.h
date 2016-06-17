0d4b9742dfea1690b870ce967214a6aed84e45ec
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-06-17 22:05:00 +0200

    CORE: align the BITS_XYZ() macros

--- include/bits.h
+++ include/bits.h
@@ -7,8 +7,8 @@
 
 #define	BITS(O, W)	(O), (W)
 
-#define	BITS_EXTRACT(VAL, BITS)	_BIT_EXTRACT(VAL, BITS)
-#define	_BIT_EXTRACT(VAL, O, W)	(((VAL) >> (O)) & MSK(W))
+#define	BITS_EXTRACT(VAL, BITS)		_BIT_EXTRACT(VAL, BITS)
+#define	_BIT_EXTRACT(VAL, O, W)		(((VAL) >> (O)) & MSK(W))
 #define	BITS_INSERT(BASE, BITS, VAL)	_BIT_INSERT(BASE, BITS, VAL)
 #define	_BIT_INSERT(BASE, O, W, VAL)	((BASE & ~MASK(O, W)) | ((VAL) << (O)))
 
