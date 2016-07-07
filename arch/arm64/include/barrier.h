5ca2a3450015b85673ef2f23c570ddeb46357036
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-07 18:43:07 +0200

    ARM64: add macros for architectural memory barriers

--- arch/arm64/include/barrier.h
+++ arch/arm64/include/barrier.h
@@ -1,6 +1,11 @@
 #ifndef	_ARCH_BARRIER_H_
 #define	_ARCH_BARRIER_H_
 
+
+#define	dmb(opt) asm volatile ("dmb " #opt : : : "memory")
+#define	dsb(opt) asm volatile ("dsb " #opt : : : "memory")
+#define	isb()    asm volatile ("isb" : : : "memory")
+
 #include <generic/barrier.h>
 
 #endif
