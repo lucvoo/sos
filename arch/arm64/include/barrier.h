f713f8dced37518b87828690c712dd090dfae736
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-07 18:48:19 +0200

    ARM64: add support for generic memory barriers

--- arch/arm64/include/barrier.h
+++ arch/arm64/include/barrier.h
@@ -6,6 +6,19 @@
 #define	dsb(opt) asm volatile ("dsb " #opt : : : "memory")
 #define	isb()    asm volatile ("isb" : : : "memory")
 
+
+#ifdef	CONFIG_SMP
+#define	mb()		dmb(sy)
+#define	mbr()		dmb(ld)
+#define	mbw()		dmb(st)
+#endif
+
+#ifdef	CONFIG_SMP
+#define	mb_smp()	dmb(ish)
+#define	mbr_smp()	dmb(ishld)
+#define	mbw_smp()	dmb(ishst)
+#endif
+
 #include <generic/barrier.h>
 
 #endif
