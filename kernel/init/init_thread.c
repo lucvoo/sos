71dbdebec257d4dd00b24ec9caf74fc61b228485
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-08 23:36:36 +0200

    THREAD: init_thread_init() never need to setup a stack
    Either we use THREAD_STACK and thus the stack is part of init_thread
    or the stack used for init_thread is __startup_stack (setup early).

--- kernel/init/init_thread.c
+++ kernel/init/init_thread.c
@@ -6,16 +6,8 @@
 struct thread init_thread[NR_CPUS] __uninit;
 
 #ifndef CONFIG_THREAD_STACK
-#include <init.h>
-#include <page.h>
-
-static unsigned char init_stack[PAGE_SIZE];
-
 static void init_thread_init(void)
 {
-	init_thread.stack_base = init_stack;
-	init_thread.stack_size = sizeof(init_stack);
-
 	set_current_thread(&init_thread[0]);
 }
 pure_initcall(init_thread_init);
