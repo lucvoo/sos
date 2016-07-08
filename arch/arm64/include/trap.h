5db632b1b0028e34f1f9e1422fe9c40c961076db
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-08 10:42:54 +0200

    CORE: make conditional traps generic

--- arch/arm64/include/trap.h
+++ arch/arm64/include/trap.h
@@ -7,10 +7,4 @@ static inline void __TRAP(int trap)
 	asm volatile("brk	%0" :: "n" (trap));
 }
 
-static inline void __TRAP_COND(int trap, int cond)
-{
-	if (cond)
-		__TRAP(trap);
-}
-
 #endif
--- include/trap.h
+++ include/trap.h
@@ -3,6 +3,10 @@
 
 #include <arch/trap.h>
 
+#ifndef	__TRAP_COND
+#define	__TRAP_COND(TRAP, COND)		do { if (COND) __TRAP(TRAP); } while (0)
+#endif
+
 #define	TRAP_UNUSED	0
 #define	TRAP_TEST	1
 #define	TRAP_BUG	2
