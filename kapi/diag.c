382792d812ff6368d35f4b3a1ff9ac025d93cbb2
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-08 03:14:19 +0200

    DIAG: remove the irq disable/restore
    The real protection is done with a lock at printf() level

--- kapi/diag.c
+++ kapi/diag.c
@@ -6,21 +6,14 @@
 void _os_diag_write_string(const char* str)
 {
 	unsigned int c;
-	unsigned long flags;
 
-	flags = __local_irq_save();
 	while ((c = *str++))
 		_os_diag_write_char(c);
-	__local_irq_rest(flags);
 }
 
 void _os_diag_write_buf(const char* str, unsigned int n)
 {
-	unsigned long flags;
-
-	flags = __local_irq_save();
 	while (n--)
 		_os_diag_write_char(*str++);
-	__local_irq_rest(flags);
 }
 weak_alias(_os_diag_write_buf, putx);
