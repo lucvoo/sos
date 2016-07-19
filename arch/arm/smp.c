b298706a580e3b567e3afd2634bc146f5c0111c0
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-19 14:33:36 +0200

    ARM: make __smp_init() generic

--- arch/arm/smp.c
+++ arch/arm/smp.c
@@ -1,59 +1,6 @@
 #include <smp.h>
-#include <smp/init.h>
-#include <smp/ops.h>
 #include <thread.h>
 
 
-#define	foreach_cpu(cpu)	for (cpu = 0; cpu < NR_CPUS; cpu++)
-
-// FIXME: should allocate this dynamically
 extern struct thread init_thread[];
 
-static int smp_boot_cpu(uint cpu)
-{
-	struct thread *idle = &init_thread[cpu];
-	int rc;
-
-	// initialize the thread with the default stack size
-	thread_create(idle, 0, NULL, NULL, NULL, 0);
-
-
-	rc = smp_ops.boot_cpu(idle, cpu);
-	return rc;
-}
-
-int __smp_init(void)
-{
-	uint cpu;
-	int rc;
-
-	rc = smp_ops.init();
-	if (rc) {
-		pr_err("mach smp init failed (%d)\n", rc);
-		return rc;
-	}
-
-	foreach_cpu(cpu) {
-		if (cpu == 0)
-			continue;
-
-		// allocate thread & stack
-
-
-		// boot the cpu
-		// this will call smp_entry()
-		// which will do:
-		//	* some generic inits
-		//	* call smp_secondary_init() for specific inits
-		//	* call smp_start()
-		rc = smp_boot_cpu(cpu);
-		if (rc) {
-			pr_err("cpu %u: booting returns %d\n", cpu, rc);
-			continue;
-		}
-
-		// FIXME: should wait till ??? is finish/released?
-	}
-
-	return 0;
-}
