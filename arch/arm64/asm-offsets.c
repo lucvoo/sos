107dfa1d7c66f7b370974525390fbbd52c943c91
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-19 16:38:42 +0200

    ARM64,SMP: avoid asm in __smp_init_idle_thread()

--- arch/arm64/asm-offsets.c
+++ arch/arm64/asm-offsets.c
@@ -10,6 +10,7 @@
 int main(void)
 {
   DEFINE(TI_CONTEXT_X0,		offsetof(struct thread, cpu_context.x[0]));
+  DEFINE(THR_CTXT_SP,		offsetof(struct thread, cpu_context.sp));
   BLANK();
   DEFINE(EFRAME_REGS,		offsetof(struct eframe, x[0]));
   DEFINE(EFRAME_LR,		offsetof(struct eframe, x[30]));
--- arch/arm64/smp.c
+++ arch/arm64/smp.c
@@ -4,20 +4,12 @@
 
 extern struct thread init_thread[];
 
-void __smp_start_idle_thread(void);
-void __smp_start_idle_thread(void)
+struct thread *__smp_start_idle_thread(void);
+struct thread *__smp_start_idle_thread(void)
 {
 	struct thread *idle = &init_thread[__coreid()];
 
-	asm volatile("\n"
-	"	mov	sp, %0\n"
-	:: "r" (idle->cpu_context.sp)
-	);
-
 	set_current_thread(idle);
 
-	asm volatile("\n"
-	"	b	__smp_start\n"
-	);
-	__builtin_unreachable();
+	return idle;
 }
