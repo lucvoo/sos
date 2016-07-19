e46c6304d8a98a3c5a4dd9424f675041088da53c
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-19 14:43:13 +0200

    ARM,SMP: __smp_start_mach() is not mach but arch specific

--- arch/arm/smp.c
+++ arch/arm/smp.c
@@ -4,3 +4,15 @@
 
 extern struct thread init_thread[];
 
+void __smp_start_idle_thread(void) __attribute__((naked));
+void __smp_start_idle_thread(void)
+{
+	struct thread *idle = &init_thread[__coreid()];
+
+	set_current_thread(idle);
+
+	asm("\n"
+	"	ldr	sp, %0\n"
+	"	b	__smp_start"
+	:: "m" (idle->cpu_context.sp));
+}
