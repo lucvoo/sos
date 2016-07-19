9e8e9e088280797ee07469663be9680440670d59
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-19 14:30:42 +0200

    SMP: make __smp_init() generic

--- arch/arm64/smp.c
+++ arch/arm64/smp.c
@@ -1,10 +1,6 @@
 #include <smp.h>
-#include <smp/init.h>
-#include <smp/ops.h>
 #include <thread.h>
 
-
-#define	foreach_cpu(cpu)	for (cpu = 0; cpu < NR_CPUS; cpu++)
 
 extern struct thread init_thread[];
 
@@ -24,47 +20,4 @@ void __smp_start_idle_thread(void)
 	"	b	__smp_start\n"
 	);
 	__builtin_unreachable();
-}
-
-int __smp_init(void)
-{
-	uint cpu;
-	int rc;
-
-	rc = smp_ops.init();
-	if (rc) {
-		pr_err("mach smp init failed (%d)\n", rc);
-		return rc;
-	}
-
-	foreach_cpu(cpu) {
-		struct thread *idle = &init_thread[cpu];
-
-		if (cpu == 0)
-			continue;
-
-		// init thread
-		rc = thread_create(idle, 0, NULL, NULL, NULL, 0);
-		if (rc) {
-			pr_err("cpu %u: can't create idle thread: %d\n", cpu, rc);
-			continue;
-		}
-
-
-		// boot the cpu
-		// this will call smp_entry()
-		// which will do:
-		//	* some generic inits
-		//	* call smp_secondary_init() for specific inits
-		//	* call smp_start()
-		rc = smp_ops.boot_cpu(idle, cpu);
-		if (rc) {
-			pr_err("cpu %u: booting returns %d\n", cpu, rc);
-			continue;
-		}
-
-		// FIXME: should wait till ??? is finish/released?
-	}
-
-	return 0;
 }
--- kernel/smp/init.c
+++ kernel/smp/init.c
@@ -7,6 +7,48 @@
 #include <sched.h>
 
 
+#define	foreach_cpu(cpu)	for (cpu = 0; cpu < NR_CPUS; cpu++)
+
+extern struct thread init_thread[];
+
+int __weak __smp_init(void)
+{
+	uint cpu;
+	int rc;
+
+	rc = smp_ops.init();
+	if (rc) {
+		pr_err("mach smp init failed (%d)\n", rc);
+		return rc;
+	}
+
+	foreach_cpu(cpu) {
+		struct thread *idle = &init_thread[cpu];
+
+		if (cpu == 0)
+			continue;
+
+		// init thread
+		rc = thread_create(idle, 0, NULL, NULL, NULL, 0);
+		if (rc) {
+			pr_err("cpu %u: can't create idle thread: %d\n", cpu, rc);
+			continue;
+		}
+
+		// boot the cpu:
+		rc = smp_ops.boot_cpu(idle, cpu);
+		if (rc) {
+			pr_err("cpu %u: booting returns %d\n", cpu, rc);
+			continue;
+		}
+
+		// FIXME: should wait till cpu is started?
+	}
+
+	return 0;
+}
+
+
 void __smp_start(void)
 {
 	uint cpu = __coreid();
