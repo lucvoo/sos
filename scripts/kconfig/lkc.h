43f855db1a63d08e1f6b01cd5906032be0a914d8
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-07-20 23:18:04 +0200

    KCONF: avoid useless warning about statements without effect

--- scripts/kconfig/lkc.h
+++ scripts/kconfig/lkc.h
@@ -12,8 +12,8 @@
 # include <libintl.h>
 #else
 # define gettext(Msgid) ((const char *) (Msgid))
-# define textdomain(Domainname) ((const char *) (Domainname))
-# define bindtextdomain(Domainname, Dirname) ((const char *) (Dirname))
+# define textdomain(Domainname)			do ; while (0)
+# define bindtextdomain(Domainname, Dirname)	do ; while (0)
 #endif
 
 #ifdef __cplusplus
