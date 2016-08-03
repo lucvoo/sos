e48ee81b401f1e6341950e9a18cfdd4ab9f41a87
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-08-03 22:12:36 +0200

    SEMA,TEST: simplify the test

--- tests/sema-smp.test.c
+++ tests/sema-smp.test.c
@@ -8,27 +8,89 @@
 #include <hz.h>
 
 
-#define	started(name)	printf("%d: thr0 (%p) started\n", __coreid(), get_current_thread())
+#define	started(name)	printf("%d: %s (%p) started\n", __coreid(), name, get_current_thread())
 
+
+
+
+static void do_loop_wait(struct semaphore *sem, const char *name, uint delay)
+{
+	while (1) {
+		printf("%d: %s sleep ...\n", __coreid(), name);
+		thread_schedule_timeout(delay);
+		printf("%d: %s awake!\n", __coreid(), name);
+
+		printf("%d: %s try ...\n", __coreid(), name);
+		semaphore_wait(sem);
+		printf("%d: %s got!\n", __coreid(), name);
+
+		printf("%d: %s rel\n", __coreid(), name);
+		semaphore_post(sem);
+	}
+}
+
+static void fun1(void *data)
+{
+	struct semaphore *sem = data;
+	const char *name = "thr1";
+
+	started(name);
+
+	// Will unblock thr0
+	printf("%d: %s rel\n", __coreid(), name);
+	semaphore_post(sem);
+
+	do_loop_wait(sem, name, HZ/4);
+}
+
+static void fun2(void *data)
+{
+	struct semaphore *sem = data;
+	const char *name = "thr2";
+
+	started(name);
+
+	do_loop_wait(sem, name, 3*HZ/16);
+}
+
+
+static void do_loop_yield(const char *name)
+{
+	started(name);
+
+	while (1) {
+		//printf("%d: %s running\n", __coreid(), name);
+		thread_yield();
+	}
+}
+
+/******************************************************************************/
+static struct thread thr1 __uninit;
+static struct thread thr2 __uninit;
 
 static void fun0(void* data)
 {
 	struct semaphore *sem = data;
+	const char *name = "thr0";
 	int n = 0;
 
-	started("thr0");
+	started(name);
+
 	__smp_init();
+	thread_create(&thr1, 2, fun1, sem, NULL, 0);
+	thread_create(&thr2, 2, fun2, sem, NULL, 0);
+	thread_start(&thr2);
+	thread_start(&thr1);
 
 	/*
 	 * The first wait will block until thr1, on the other cores, is up & running
 	 */
 	while (1) {
-		printf("%d: thr0 run %d\n", __coreid(), ++n);
-		printf("%d: thr0 try ...\n", __coreid());
+		printf("%d: %s try %d ...\n", __coreid(), name, ++n);
 		semaphore_wait(sem);
-		printf("%d: thr0 got!\n", __coreid());
+		printf("%d: %s got!\n", __coreid(), name);
 		thread_yield();
-		printf("%d: thr0 rel\n", __coreid());
+		printf("%d: %s rel\n", __coreid(), name);
 		semaphore_post(sem);
 	};
 }
@@ -39,92 +101,15 @@ static struct thread thr0 __uninit;
 
 void kapi_start(void)
 {
+	printf("%d: started (idle = %p)\n", __coreid(), get_current_thread());
 
-	printf(os_version);
-
-	printf("0: idle = %p\n", get_current_thread());
 	semaphore_init(&sema, 0);
 
 	thread_create(&thr0, 2, fun0, &sema, NULL, 0);
 	thread_start(&thr0);
 }
 
-
-static void fun1(void *data)
-{
-	struct semaphore *sem = data;
-
-	started("thr1");
-
-	// Will unblock thr0
-	semaphore_post(sem);
-
-	while (1) {
-		printf("%d: thr1 sleep ...\n", __coreid());
-		thread_schedule_timeout(HZ/4);
-		printf("%d: thr1 awake!\n", __coreid());
-		printf("%d: thr1 try ...\n", __coreid());
-		semaphore_wait(sem);
-		printf("%d: thr1 got!\n", __coreid());
-		printf("%d: thr1 rel\n", __coreid());
-		semaphore_post(sem);
-	}
-}
-
-static void fun2(void *data)
-{
-	struct semaphore *sem = data;
-
-	started("thr2");
-
-	while (1) {
-		printf("%d: thr2 try ...\n", __coreid());
-		semaphore_wait(sem);
-		printf("%d: thr2 got!\n", __coreid());
-		printf("%d: thr2 sleep ...\n", __coreid());
-		thread_schedule_timeout(3*HZ/16);
-		printf("%d: thr2 awake!\n", __coreid());
-		printf("%d: thr2 rel\n", __coreid());
-		semaphore_post(sem);
-	}
-}
-
-static void fun3(void *data)
-{
-	started("thr3");
-
-	while (1) {
-		//printf("%d: thr3 running\n", __coreid());
-		thread_yield();
-	}
-}
-
-static void fun4(void *data)
-{
-	started("thr4");
-
-	while (1) {
-		//printf("%d: thr4 running\n", __coreid());
-		thread_yield();
-	}
-}
-
-static struct thread thr1 __uninit;
-static struct thread thr2 __uninit;
-static struct thread thr3 __uninit;
-static struct thread thr4 __uninit;
-
 void kapi_start_smp(void)
 {
-	printf("%d: SMP started\n", __coreid());
-	printf("%d: idle = %p\n", __coreid(), get_current_thread());
-
-	thread_create(&thr1, 2, fun1, &sema, NULL, 0);
-	thread_create(&thr2, 2, fun2, &sema, NULL, 0);
-	thread_create(&thr3, 2, fun3, &sema, NULL, 0);
-	thread_create(&thr4, 2, fun4, &sema, NULL, 0);
-	thread_start(&thr2);
-	thread_start(&thr1);
-	thread_start(&thr3);
-	thread_start(&thr4);
+	printf("%d: started (idle = %p)\n", __coreid(), get_current_thread());
 }
