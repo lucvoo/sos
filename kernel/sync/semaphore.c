647515a03126a7a211203f899d76298598d45ec4
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-08-04 04:28:05 +0200

    SEMA: *_wait() need IRQ safe locking

--- kernel/sync/semaphore.c
+++ kernel/sync/semaphore.c
@@ -14,10 +14,10 @@ void __semaphore_wait_slowpath(struct semaphore* sem)
 	struct thread* curr = get_current_thread();
 	struct waiter w;
 
-	lock_acq(&sem->wq.lock);
+	lock_acq_irq(&sem->wq.lock);
 	curr->state = THREAD_STATE_SLEEPING;
 	waitqueue_add(&sem->wq, &w, curr);
-	lock_rel(&sem->wq.lock);
+	lock_rel_irq(&sem->wq.lock);
 
 	// FIXME: we *MUST* check the reason for the wakeup
 	thread_schedule();
