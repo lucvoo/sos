8fffbae1594f951e2f4ede306a67bb965127f72e
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-08-13 06:25:05 +0200

    MIPS,GIC: add defines for the GIC

---
+++ arch/mips/include/gic-irqs.h
@@ -0,0 +1,12 @@
+#ifndef _ARCH_GIC_IRQS_H_
+#define _ARCH_GIC_IRQS_H_
+
+#define GIC_WD		0
+#define GIC_COMP	1
+#define GIC_TIMER	2
+#define GIC_PCI		3
+#define GIC_SW0		4
+#define GIC_SW1		5
+#define GIC_FDC		6
+
+#endif
