760d0ed12c48b09d7ac27ef142c7c4a6ec0f53fd
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-09-04 17:23:51 +0200

    CORE: extract u64/u32+u32 union

--- include/64bit.h
+++ include/64bit.h
@@ -2,24 +2,27 @@
 #define	_64BIT_H_
 
 
+typedef union {
+	struct {
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+	u32	hi;
+#endif
+	u32	lo;
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+	u32	hi;
+#endif
+	};
+	u64	hl;
+} u2x32_t;
+
+
 static inline u64 mk_u64(u32 hi, u32 lo)
 {
-	/*
-	 * This function only exist because gcc generate horrible code
-	 * when using a cast-shift-{or,add} combo (at least on ARM).
-	 */
-	union {
-		unsigned long long	hilo;
-		struct {
-#if	__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-			unsigned int lo, hi;
-#elif	__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-			unsigned int hi, lo;
-#endif
-		};
-	}  res = { .lo = lo, .hi = hi, };
+	// This function only exists because gcc generate horrible code
+	// when using a cast-shift-{or,add} combo (at least on ARM).
+	u2x32_t res = { .lo = lo, .hi = hi, };
 
-	return res.hilo;
+	return res.hl;
 }
 
 #endif
