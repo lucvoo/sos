547883709b5ddac8027202e89372eecd67098536
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-09-05 14:53:59 +0200

    SMP: remove IPI testing values

--- include/smp/ipi.h
+++ include/smp/ipi.h
@@ -6,9 +6,7 @@
 #define	SMP_IPI_OTHER	-2
 #define	SMP_IPI_SELF	-3
 
-#define	SMP_IPI_PING		0
-#define	SMP_IPI_PONG		1
-#define	SMP_IPI_SCHEDULE	2
+#define	SMP_IPI_SCHEDULE 0
 
 void __smp_ipi_send(uint cpu, uint msg);
 void __smp_ipi_process(uint ipi);
