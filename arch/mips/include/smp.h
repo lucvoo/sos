e2918169e93d18a44280aad73a7bd65cf2866a1f
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2016-09-11 17:13:42 +0200

    MIPS,SMP: add SMP macros for MIPS MT

--- arch/mips/include/smp.h
+++ arch/mips/include/smp.h
@@ -7,6 +7,18 @@
 
 #define	__corenum()	(c0_getval(c0_ebase) & 0x000003ff)
 
+#ifdef	CONFIG_MIPS_MT	// core == cpu
+
+#define	NR_CPUS		NR_CORES
+#define	__cpuid()	(__corenum() >> 1)
+#define	__hwcpu(cpu)	((cpu) << 1)
+#define	__vpeid(N)	0
+#define	__coreid(N)	(N)
+
+#else	// CONFIG_MIPS_MT
+
 #define	__cpuid()	__corenum()
+
+#endif	// CONFIG_MIPS_MT
 
 #endif
