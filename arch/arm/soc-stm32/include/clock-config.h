41f7c1abeb0bfa95b2800cb1c663121afbedea34
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2019-08-11 17:38:49 +0200

    stm32: rewrite the clock configuration

--- arch/arm/soc-stm32/include/clock-config.h
+++ arch/arm/soc-stm32/include/clock-config.h
@@ -3,34 +3,46 @@
 
 #include <soc/rcc.h>
 
-#if	defined(CONFIG_STM32_CLK_12_168)
-#define HSE	12
-#define SYSCLK	168
+/*
+ * - f(VCOin)  = f(PLLin) / PLLM
+ * - f(VCOout) = f(VCOin) × PLLN	(192 ≤ VCOout ≤ 432)
+ * - f(PLLout) = f(VCOout) / PLLP	(< 168 or 180MHz)
+ * - f(USB OTG FS, SDIO, RNG clock output) = f(VCOout) / PLLQ
+ *
+ * Normally, PLLin is HSE (High-Speed External) and
+ * SYSCLK is PLLout
+ */
+
+#if 0
+#elif CONFIG_STM32_PLL_MAX >= 168
+#define HSE	CONFIG_STM32_HSE_FREQ
+#define PLLM	(HSE / 2)
+#define PLLN	168
+#if CONFIG_STM32_CLK_MAX < 168
+#define PLLP	3
+#else
+#define PLLP	2
+#endif
+#define PLLQ	7
 #define	AHBDIV	1
 #define HPRE	RCC_CFGR_HPRE_1
 #define	APB1DIV	4
 #define PPRE1	RCC_CFGR_PPRE1_4
 #define	APB2DIV	2
 #define PPRE2	RCC_CFGR_PPRE2_2
-#define PLLM	6
-#define PLLN	336
-#define PLLP	4
-#define PLLQ	14
 #define WS	5
-#elif	defined(CONFIG_STM32_CLK_8_180)
-#define HSE	8
-#define SYSCLK	180
-#define	AHBDIV	1
-#define HPRE	RCC_CFGR_HPRE_1
-#define	APB1DIV	4
-#define PPRE1	RCC_CFGR_PPRE1_4
-#define	APB2DIV	2
-#define PPRE2	RCC_CFGR_PPRE2_2
-#define PLLM	4
-#define PLLN	360
-#define PLLP	4
-#define PLLQ	15
-#define WS	5
+#endif
+
+#define	VCOIN	(HSE / PLLM)
+#define	VCOOUT	(VCOIN * PLLN)
+#define	PLLOUT	(VCOOUT / PLLP)
+#define SYSCLK	(PLLOUT)
+
+#if VCOOUT < 192 || VCOOUT > 432
+#error "invalid VCOout"
+#endif
+#if PLLOUT > CONFIG_STM32_CLK_MAX
+#error "PLLout is too high"
 #endif
 
 #define	HCLK		(SYSCLK/AHBDIV)
